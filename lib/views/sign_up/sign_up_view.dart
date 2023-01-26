93b581bf7b6fe5e47938866f19440ca1b7c33e79
Kristijan <kmitrikjeski@gmail.com>
2023-01-26 12:19:05 +0100

    Move sign up components to a separate folder

--- lib/views/sign_up/sign_up_view.dart
+++ lib/views/sign_up/sign_up_view.dart
@@ -3,7 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mysterium_vpn/common/styles/palette.dart';
 import 'package:mysterium_vpn/components/easy_text.dart';
 import 'package:mysterium_vpn/providers/state_providers.dart';
-import 'package:mysterium_vpn/views/sign_up_form.dart';
+import 'package:mysterium_vpn/views/sign_up/sign_up_form.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 class SignUpView extends HookConsumerWidget {
