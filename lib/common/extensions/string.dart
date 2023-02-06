e84566a2279097062fc9a9d4a1939f62bdaa00c5
Kristijan <kmitrikjeski@gmail.com>
2023-02-06 16:17:04 +0100

    Added date extension

--- lib/common/extensions/string.dart
+++ lib/common/extensions/string.dart
@@ -1,18 +1,3 @@
-import 'package:flutter/material.dart';
-
-extension LanguageName on Locale {
-  String get languageName {
-    switch (languageCode) {
-      case 'en':
-        return 'English';
-
-      case 'es':
-        return 'Español';
-    }
-    return '';
-  }
-}
-
 extension StringExtensions on String {
   bool isUpperCase() {
     return this == toUpperCase();
