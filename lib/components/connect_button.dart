d5843549b7142bdc36e049f0584c6b33ab877d05
Kristijan <kmitrikjeski@gmail.com>
2023-02-06 12:27:50 +0100

    Format code properly

--- lib/components/connect_button.dart
+++ lib/components/connect_button.dart
@@ -5,7 +5,11 @@ import 'package:mysterium_vpn/components/svg_icon_button.dart';
 
 class ConnectButton extends StatelessWidget {
   const ConnectButton(
-      {required this.isConnected, required this.callback, required this.height, required this.width, super.key});
+      {required this.isConnected,
+      required this.callback,
+      required this.height,
+      required this.width,
+      super.key});
 
   final bool isConnected;
   final VoidCallback callback;
