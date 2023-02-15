778035b09dd139a346d6644cad27390d02eebe63
Kristijan <kmitrikjeski@gmail.com>
2023-02-15 13:27:20 +0100

    Implement report page for mobile

--- lib/pages/report_issue_page.dart
+++ lib/pages/report_issue_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mysterium_vpn/common/layout_builders/screen_type_builder.dart';
 import 'package:mysterium_vpn/components/colored_scaffold.dart';
-import 'package:mysterium_vpn/views/settings/settings_desktop_view.dart';
-import 'package:mysterium_vpn/views/settings/settings_mobile_view.dart';
+import 'package:mysterium_vpn/views/report_issue/report_issue_desktop_view.dart';
+import 'package:mysterium_vpn/views/report_issue/report_issue_mobile_view.dart';
 
 class ReportIssuePage extends StatelessWidget {
   const ReportIssuePage({super.key});
@@ -11,9 +11,9 @@ class ReportIssuePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ColoredScaffold(
       body: ScreenTypeLayoutBuilder(
-        mobile: (BuildContext context) => const SettingsMobileView(),
-        tablet: (BuildContext context) => const SettingsDesktopView(),
-        desktop: (BuildContext context) => const SettingsDesktopView(),
+        mobile: (BuildContext context) => const ReportIssueMobileView(),
+        tablet: (BuildContext context) => const ReportIssueDesktopView(),
+        desktop: (BuildContext context) => const ReportIssueDesktopView(),
       ),
     );
   }
