02e0cd3e0b9e56f8bb3051b745d1cdd23b6284da
Kristijan <kmitrikjeski@gmail.com>
2023-02-15 10:30:43 +0100

    Add report issue page and navigation

---
+++ lib/views/report_issue/report_issue_mobile_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class ReportIssueMobileView extends HookConsumerWidget {
+  const ReportIssueMobileView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Center(
+      child: Text('Report issue Mobile View'),
+    );
+  }
+}
