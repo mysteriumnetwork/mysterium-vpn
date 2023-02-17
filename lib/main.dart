e7d4ca4c832f76aef9448285ef1f6408749fa225
Kristijan <kmitrikjeski@gmail.com>
2023-02-17 15:45:30 +0100

    Add native splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mysterium_vpn/app.dart';
 import 'package:mysterium_vpn/common/constants/constants.dart';
 import 'package:mysterium_vpn/services/shared_preferences_service.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await SharedPreferenceService.init();
   await EasyLocalization.ensureInitialized();
-
+  FlutterNativeSplash.remove();
   runApp(
     ProviderScope(
       child: EasyLocalization(
