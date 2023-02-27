5bd2bef57045c8edb91306289679c47858d5ac0e
Kristijan <kmitrikjeski@gmail.com>
2023-02-27 15:21:58 +0100

    Refactor connect button

--- lib/components/connect_button.dart
+++ lib/components/connect_button.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mysterium_vpn/common/styles/assets.dart';
-import 'package:mysterium_vpn/common/styles/palette.dart';
-import 'package:mysterium_vpn/components/circle_box.dart';
 import 'package:mysterium_vpn/components/svg_icon_button.dart';
 
 class ConnectButton extends StatelessWidget {
@@ -18,15 +16,8 @@ class ConnectButton extends StatelessWidget {
   final double width;
   final double height;
   @override
-  Widget build(BuildContext context) {
-    final radius = ((height + width) / 2) * 0.23;
-    return CircleBox(
-      size: radius,
-      color: isConnected ? Palette.purple : Palette.lightBlack,
-      child: SvgIconButton(
+  Widget build(BuildContext context) => SvgIconButton(
         onPressed: callback,
         asset: Assets.connectButton,
-      ),
-    );
-  }
+      );
 }
