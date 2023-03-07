6a385348269a5b19c65ee2abd3af31bbe9d3b5b1
Kristijan <kmitrikjeski@gmail.com>
2023-03-07 16:37:02 +0100

    Create subscription form and product items

---
+++ lib/models/purchasable_product.dart
@@ -0,0 +1,55 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:mobx/mobx.dart';
+import 'package:mysterium_vpn/common/constants/constants.dart';
+import 'package:mysterium_vpn/generated/locale_keys.g.dart';
+
+part 'purchasable_product.g.dart';
+
+// ignore: library_private_types_in_public_api
+class PurchasableProduct = _PurchasableProduct with _$PurchasableProduct;
+
+abstract class _PurchasableProduct with Store {
+  _PurchasableProduct({required this.productDetails, required this.status});
+
+  final ProductDetails productDetails;
+
+  @observable
+  ProductStatus status;
+
+  @computed
+  String get id => productDetails.id.replaceAll('-', '_');
+  @computed
+  String get title => productDetails.title;
+  @computed
+  String get description => productDetails.description;
+  @computed
+  String get fullPrice => productDetails.price;
+  @computed
+  String get originalMonthlyPrice => productDetails.id != kMonthlyPlan ? r'$9.99  ' : '';
+  @computed
+  String get monthlyPrice => productDetails.id == kMonthlyPlan
+      ? r'$9.99'
+      : productDetails.id == kAnnualPlan
+          ? r'$4.99'
+          : r'$6.99';
+  @computed
+  bool get isPupular => productDetails.id == kPopularPlan;
+  @computed
+  String get billedInTotal => LocaleKeys.billedInTotal.tr(
+        namedArgs: {
+          'amount': fullPrice,
+          'period': productDetails.id == kMonthlyPlan
+              ? LocaleKeys.monthly.tr()
+              : productDetails.id == ksemiAnnualPlan
+                  ? LocaleKeys.semiAnnual.tr()
+                  : LocaleKeys.yearly.tr(),
+        },
+      );
+}
+
+enum ProductStatus {
+  purchasable,
+  purchased,
+  pending,
+}
--- lib/views/subscription/subscription_mobile_view.dart
+++ lib/views/subscription/subscription_mobile_view.dart
@@ -1,12 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:mysterium_vpn/common/styles/palette.dart';
+import 'package:mysterium_vpn/common/enums/enums.dart';
 import 'package:mysterium_vpn/components/base_layout.dart';
-import 'package:mysterium_vpn/components/easy_text.dart';
+import 'package:mysterium_vpn/components/error_widget.dart';
+import 'package:mysterium_vpn/components/loading_indicator.dart';
 import 'package:mysterium_vpn/generated/locale_keys.g.dart';
 import 'package:mysterium_vpn/providers/state_providers.dart';
 import 'package:mysterium_vpn/views/subscription/subscription_app_bar.dart';
+import 'package:mysterium_vpn/views/subscription/subscription_form.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 class SubscriptionMobileView extends ConsumerWidget {
@@ -15,16 +18,29 @@ class SubscriptionMobileView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final authStore = ref.watch(authStorePOD);
+    final subscriptionStore = ref.watch(subscriptionStorePOD);
 
     return BaseLayout(
       header: SubscriptionAppBar(authStore: authStore),
-      child: Center(
-        child: EasyText(
-          LocaleKeys.subscription_desktop.tr(),
-          maxLines: 4,
-          color: Palette.purple,
-          textAlign: TextAlign.center,
-        ).paddingDirectional(all: 20),
+      child: Observer(
+        builder: (context) => Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (subscriptionStore.isAvailable == StoreState.loading)
+              LoadingIndicator(
+                message: LocaleKeys.connectingToPaymentProcesor.tr(),
+              )
+            else if (subscriptionStore.isAvailable == StoreState.notAvailable)
+              RetryOnErrorWidget(
+                error: LocaleKeys.unableToConnectToPaymentProcesor.tr(),
+                onRetry: subscriptionStore.checkAvailability,
+              )
+            else
+              SubscriptionForm(
+                store: subscriptionStore,
+              ).expanded()
+          ],
+        ),
       ),
     );
   }
