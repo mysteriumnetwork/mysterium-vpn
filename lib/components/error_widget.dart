3499c746a2e2f4880d2fe651d6d1fe201a2bed5a
Kristijan <kmitrikjeski@gmail.com>
2023-03-07 16:31:08 +0100

    Add loading and error indicator

---
+++ lib/components/error_widget.dart
@@ -0,0 +1,32 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:mysterium_vpn/common/styles/palette.dart';
+import 'package:mysterium_vpn/components/easy_button.dart';
+import 'package:mysterium_vpn/components/easy_text.dart';
+import 'package:mysterium_vpn/generated/locale_keys.g.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+class RetryOnErrorWidget extends StatelessWidget {
+  const RetryOnErrorWidget({
+    required this.error,
+    required this.onRetry,
+    super.key,
+  });
+  final String error;
+  final VoidCallback onRetry;
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          EasyText(
+            error,
+            maxLines: 4,
+            color: Palette.pink,
+            textAlign: TextAlign.center,
+          ).padding(bottom: 20),
+          EasyButton(
+            text: LocaleKeys.tryAgainBtn.tr(),
+            onPressed: onRetry,
+          ),
+        ],
+      ).paddingDirectional(all: 20).center();
+}
