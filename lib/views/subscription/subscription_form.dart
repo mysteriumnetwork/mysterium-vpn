fc6b61d96f38764c6dd8c964f837aa437982e60a
Kristijan <kmitrikjeski@gmail.com>
2023-03-10 11:13:25 +0100

    Add UI components
    Email communications form
    Notifications form

--- lib/views/subscription/subscription_form.dart
+++ lib/views/subscription/subscription_form.dart
@@ -1,3 +1,4 @@
+import 'package:beamer/beamer.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -5,6 +6,8 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mobx/mobx.dart';
 import 'package:mysterium_vpn/common/constants/constants.dart';
+import 'package:mysterium_vpn/common/enums/routes.dart';
+import 'package:mysterium_vpn/common/extensions/extensions.dart';
 import 'package:mysterium_vpn/common/styles/palette.dart';
 import 'package:mysterium_vpn/common/utils/utils.dart';
 import 'package:mysterium_vpn/components/easy_button.dart';
@@ -23,7 +26,7 @@ class SubscriptionForm extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedProduct = useState(store.purchasedProductId ?? kPopularPlan);
-
+    final isMounted = useIsMounted();
     return Column(
       children: [
         HeaderTitle(
@@ -64,27 +67,43 @@ class SubscriptionForm extends HookConsumerWidget {
                   maxLines: 3,
                   textAlign: TextAlign.center,
                 ).padding(bottom: getMediaHeight(context) * 0.025),
-                EasyButton(
-                  useSystemColor: false,
-                  color: store.isSubscribing ? Theme.of(context).disabledColor : Palette.purple,
-                  onPressed: store.isSubscribing
-                      ? null
-                      : () {
-                          if (selectedProduct.value.isNotEmpty) {
-                            store.subscribeToPackage(selectedProduct.value);
-                          }
-                        },
-                  text: store.purchasedProductId != null
-                      ? selectedProduct.value == store.purchasedProductId
-                          ? LocaleKeys.manageBtn.tr()
-                          : LocaleKeys.changeSubPlan.tr()
-                      : LocaleKeys.startTrialBtn.tr(),
+                ReactionBuilder(
+                  builder: (context) => reaction((_) => store.isSubscribing, (result) {
+                    if (result == false && isMounted()) {
+                      context.beamToNamed(Routes.emailCommunications.toRoute);
+                    }
+                  }),
+                  child: EasyButton(
+                    width: getMediaWidth(context) * 0.8,
+                    useSystemColor: false,
+                    color: store.isSubscribing ? Theme.of(context).disabledColor : Palette.purple,
+                    onPressed: store.isSubscribing
+                        ? null
+                        : () async {
+                            if (selectedProduct.value.isNotEmpty) {
+                              store.subscribeToPackage(selectedProduct.value);
+                            }
+                          },
+                    child: !store.isSubscribing
+                        ? EasyText(
+                            store.purchasedProductId != null
+                                ? selectedProduct.value == store.purchasedProductId
+                                    ? LocaleKeys.manageBtn.tr()
+                                    : LocaleKeys.changeSubPlan.tr()
+                                : LocaleKeys.startTrialBtn.tr(),
+                            color: Palette.white,
+                          )
+                        : const LoadingIndicator(
+                            radius: 20,
+                            strokeWidth: 1.5,
+                          ),
+                  ),
                 ),
               ],
             );
           },
         ),
       ],
-    );
+    ).scrollable().padding(horizontal: 20);
   }
 }
