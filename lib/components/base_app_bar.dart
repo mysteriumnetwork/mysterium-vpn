54defab6cf7e354827a31faadc02b064aac684ea
Kristijan <kmitrikjeski@gmail.com>
2023-03-10 11:10:32 +0100

    Create base app bar

--- lib/components/base_app_bar.dart
+++ lib/components/base_app_bar.dart
@@ -1,3 +1,4 @@
+import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 import 'package:mysterium_vpn/common/styles/assets.dart';
 import 'package:mysterium_vpn/components/app_logo.dart';
@@ -5,18 +6,20 @@ import 'package:mysterium_vpn/components/svg_icon_button.dart';
 import 'package:mysterium_vpn/stores/auth_store.dart';
 import 'package:styled_widget/styled_widget.dart';
 
-class SubscriptionAppBar extends StatelessWidget {
-  const SubscriptionAppBar({
+class BaseAppBar extends StatelessWidget {
+  const BaseAppBar({
     required this.authStore,
+    this.onBackButtonPressed,
     super.key,
   });
   final AuthStore authStore;
+  final VoidCallback? onBackButtonPressed;
   @override
   Widget build(BuildContext context) => Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           SvgIconButton(
-            onPressed: authStore.logout,
+            onPressed: onBackButtonPressed ?? context.beamBack,
             asset: Assets.navigateBack,
           ),
           const AppLogo(),
