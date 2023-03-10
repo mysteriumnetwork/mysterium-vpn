a5a6ac0e62f7699c1f6e8a69fa7423445ea58edd
Kristijan <kmitrikjeski@gmail.com>
2023-03-10 11:11:04 +0100

    Add new pages to the router

--- lib/pages/notifications_page.dart
+++ lib/pages/notifications_page.dart
@@ -3,20 +3,20 @@ import 'package:mysterium_vpn/common/layout_builders/platform_type_builder.dart'
 import 'package:mysterium_vpn/components/colored_scaffold.dart';
 import 'package:mysterium_vpn/generated/locale_keys.g.dart';
 import 'package:mysterium_vpn/views/informational_view.dart';
-import 'package:mysterium_vpn/views/subscription/subscription_mobile_view.dart';
+import 'package:mysterium_vpn/views/notifications/notifications_mobile_view.dart';
 
-class SubscriptionPage extends StatelessWidget {
-  const SubscriptionPage({super.key});
+class NotificationsPage extends StatelessWidget {
+  const NotificationsPage({super.key});
 
   @override
   Widget build(BuildContext context) => ColoredScaffold(
         body: PlatformTypeLayoutBuilder(
           windows: (BuildContext context) =>
-              const InformationalView(translationKey: LocaleKeys.subscriptionDesktop),
-          android: (BuildContext context) => const SubscriptionMobileView(),
+              const InformationalView(translationKey: LocaleKeys.notificationsDesktop),
+          android: (BuildContext context) => const NotificationsMobileView(),
           macos: (BuildContext context) =>
-              const InformationalView(translationKey: LocaleKeys.subscriptionDesktop),
-          ios: (BuildContext context) => const SubscriptionMobileView(),
+              const InformationalView(translationKey: LocaleKeys.notificationsDesktop),
+          ios: (BuildContext context) => const NotificationsMobileView(),
         ),
       );
 }
