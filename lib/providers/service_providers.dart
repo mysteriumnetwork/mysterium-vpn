7902d6ea7e6c167a3eaa67a5465f961ddb696ca0
Kristijan <kmitrikjeski@gmail.com>
2023-03-24 14:24:04 +0100

    Implement api services and improve subscription service

--- lib/providers/service_providers.dart
+++ lib/providers/service_providers.dart
@@ -1,15 +1,18 @@
 // import 'package:hooks_riverpod/hooks_riverpod.dart';
 //import 'package:wireguard_dart/wireguard_dart.dart';
 
-// //service providers
-
-// final wireguardServicePOD = Provider((ref) => WireguardDart());
-
+import 'package:app_links/app_links.dart';
 import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:mysterium_vpn/common/interceptors/append_auth_token.dart';
+import 'package:mysterium_vpn/common/interceptors/log_errors.dart';
+import 'package:mysterium_vpn/providers/state_providers.dart';
 import 'package:mysterium_vpn/services/api/api_service.dart';
 import 'package:mysterium_vpn/services/api/rest_api_service.dart';
+import 'package:mysterium_vpn/services/auth/auth_service.dart';
+import 'package:mysterium_vpn/services/auth/rest_auth_service.dart';
+import 'package:mysterium_vpn/services/local_db_service.dart';
 import 'package:mysterium_vpn/services/subscription/rest_subscription_service.dart';
 import 'package:mysterium_vpn/services/subscription/subscription_service.dart';
 
@@ -17,22 +20,73 @@ final inAppPurchasePOD = Provider(
   (ref) => InAppPurchase.instance,
 );
 
-final dioPOD = Provider(
-  (ref) => Dio(
+final appLinksPOD = Provider(
+  (ref) => AppLinks(),
+);
+
+final localDBPOD = Provider((ref) => LocalDBService());
+
+final authorizedApiClientPOD = Provider<Dio>((ref) {
+  final environment = ref.watch(environmentPOD);
+  return Dio(
     BaseOptions(
+      baseUrl: environment.values.baseUrl,
       connectTimeout: const Duration(seconds: 15),
       receiveTimeout: const Duration(seconds: 15),
     ),
-  ),
-);
+  )..interceptors.addAll([
+      AppendTokenInterceptor(ref),
+      CustomLogInterceptor(
+        responseHeader: false,
+        requestHeader: false,
+        requestBody: true,
+        responseBody: true,
+      )
+    ]);
+});
+
+final unauthorizedApiClientPOD = Provider<Dio>((ref) {
+  final environment = ref.watch(environmentPOD);
+
+  return Dio(
+    BaseOptions(
+      baseUrl: environment.values.baseUrl,
+      connectTimeout: const Duration(seconds: 15),
+      receiveTimeout: const Duration(seconds: 15),
+    ),
+  )..interceptors.add(
+      CustomLogInterceptor(
+        responseHeader: false,
+        requestHeader: false,
+        requestBody: true,
+        responseBody: true,
+      ),
+    );
+});
 
 final subscriptionServicePOD = Provider<SubscriptionService>((ref) {
-  final dio = ref.read(dioPOD);
-  final inAppPurchase = ref.read(inAppPurchasePOD);
-  return RestSubscriptionService(dio: dio, inAppPurchase: inAppPurchase);
+  final apiClient = ref.watch(authorizedApiClientPOD);
+  final localDb = ref.watch(localDBPOD);
+  final inAppPurchase = ref.watch(inAppPurchasePOD);
+  return RestSubscriptionService(
+    apiClient: apiClient,
+    inAppPurchase: inAppPurchase,
+    localDb: localDb,
+  );
 });
 
 final apiServicePOD = Provider<ApiService>((ref) {
-  final dio = ref.read(dioPOD);
-  return RestApiService(dio: dio);
+  final apiClient = ref.watch(authorizedApiClientPOD);
+  final localDb = ref.watch(localDBPOD);
+  return RestApiService(apiClient: apiClient, localDb: localDb);
+});
+
+final authServicePOD = Provider<AuthService>((ref) {
+  final apiClient = ref.watch(unauthorizedApiClientPOD);
+  final environment = ref.watch(environmentPOD);
+
+  return RestAuthService(
+    apiClient: apiClient,
+    scheme: environment.values.scheme,
+  );
 });
