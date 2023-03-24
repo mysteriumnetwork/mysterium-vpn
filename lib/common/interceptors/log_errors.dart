1d4bc6982dbc8516d667958150e8ea1ec2b95d15
Kristijan <kmitrikjeski@gmail.com>
2023-03-24 14:19:06 +0100

    Add auth token and log interceptors

---
+++ lib/common/interceptors/log_errors.dart
@@ -0,0 +1,119 @@
+import 'package:dio/dio.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+
+class CustomLogInterceptor extends Interceptor {
+  CustomLogInterceptor({
+    this.request = true,
+    this.requestHeader = true,
+    this.requestBody = false,
+    this.responseHeader = true,
+    this.responseBody = false,
+    this.error = true,
+    this.logPrint = print,
+  });
+
+  bool request;
+
+  bool requestHeader;
+
+  bool requestBody;
+
+  bool responseBody;
+
+  bool responseHeader;
+
+  bool error;
+
+  void Function(Object object) logPrint;
+
+  @override
+  Future<void> onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    logPrint('*** Request ***');
+    _printKV('uri', options.uri);
+    //options.headers;
+
+    if (request) {
+      _printKV('method', options.method);
+      _printKV('responseType', options.responseType.toString());
+      _printKV('followRedirects', options.followRedirects);
+      _printKV('persistentConnection', options.persistentConnection);
+      _printKV('connectTimeout', options.connectTimeout);
+      _printKV('sendTimeout', options.sendTimeout);
+      _printKV('receiveTimeout', options.receiveTimeout);
+      _printKV(
+        'receiveDataWhenStatusError',
+        options.receiveDataWhenStatusError,
+      );
+      _printKV('extra', options.extra);
+    }
+    if (requestHeader) {
+      logPrint('headers:');
+      options.headers.forEach((key, v) => _printKV(' $key', v));
+    }
+    if (requestBody) {
+      logPrint('data:');
+      _printAll(options.data);
+    }
+    logPrint('');
+
+    handler.next(options);
+  }
+
+  @override
+  Future<void> onResponse(Response response, ResponseInterceptorHandler handler) async {
+    logPrint('*** Response ***');
+    _printResponse(response);
+    handler.next(response);
+  }
+
+  @override
+  Future<void> onError(DioError err, ErrorInterceptorHandler handler) async {
+    FirebaseCrashlytics.instance.recordError(
+      err,
+      StackTrace.fromString('ApiClient'),
+      reason: 'API EXCEPTION',
+      fatal: true,
+      printDetails: true,
+    );
+    if (error) {
+      logPrint('*** DioError ***:');
+      logPrint('uri: ${err.requestOptions.uri}');
+      logPrint('$err');
+      if (err.response != null) {
+        _printResponse(err.response!);
+      }
+      logPrint('');
+    }
+
+    handler.next(err);
+  }
+
+  void _printResponse(Response response) {
+    _printKV('uri', response.requestOptions.uri);
+    if (responseHeader) {
+      _printKV('statusCode', response.statusCode);
+      if (response.isRedirect == true) {
+        _printKV('redirect', response.realUri);
+      }
+
+      logPrint('headers:');
+      response.headers.forEach((key, v) => _printKV(' $key', v.join('\r\n\t')));
+    }
+    if (responseBody) {
+      logPrint('Response Text:');
+      _printAll(response.toString());
+    }
+    logPrint('');
+  }
+
+  void _printKV(String key, Object? v) {
+    logPrint('$key: $v');
+  }
+
+  void _printAll(msg) {
+    msg.toString().split('\n').forEach(logPrint);
+  }
+}
