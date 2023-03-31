647487af3f9ecc0b2850d6b3b439eca1694bc36b
Kristijan <kmitrikjeski@gmail.com>
2023-03-31 12:18:46 +0200

    Set default flavour to DEV
    Will run DEV flavour if the app is started from xcode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:mysterium_vpn/entrypoints/enviroment.dart';
 
 void main() async {
-  const flavor = String.fromEnvironment('FLAVOR', defaultValue: 'PROD');
+  const flavor = String.fromEnvironment('FLAVOR', defaultValue: 'DEV');
   Enviroment().launch(
     flavor: flavor,
   );
