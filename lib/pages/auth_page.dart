0aec8cf4339984a8486a095a901a24db7b2508dd
Kristijan <kmitrikjeski@gmail.com>
2023-04-05 13:40:09 +0200

    Remove sign up route

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,20 +1,18 @@
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mysterium_vpn/common/enums/routes.dart';
 import 'package:mysterium_vpn/common/extensions/extensions.dart';
 import 'package:mysterium_vpn/common/router/router.dart';
-import 'package:mysterium_vpn/providers/state_providers.dart';
 
-class SignUpPage extends ConsumerWidget {
+class SignUpPage extends StatelessWidget {
   const SignUpPage({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) => Beamer(
+  Widget build(BuildContext context) => Beamer(
         routerDelegate: BeamerDelegate(
-          initialPath: ref.read(isSignedInPOD) ? Routes.signIn.toRoute : Routes.signUp.toRoute,
+          initialPath: Routes.signIn.toRoute,
           initializeFromParent: false,
           updateFromParent: false,
           updateParent: false,
