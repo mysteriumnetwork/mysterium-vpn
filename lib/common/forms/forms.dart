9a77aa48149b92a54bce0641de01182c8dbef282
Kristijan <kmitrikjeski@gmail.com>
2023-04-05 13:42:54 +0200

    Remove sign up form

--- lib/common/forms/forms.dart
+++ lib/common/forms/forms.dart
@@ -1,10 +1,5 @@
 import 'package:reactive_forms/reactive_forms.dart';
 
-FormGroup singUp() => FormGroup({
-      'email': FormControl<String>(validators: [Validators.required, Validators.email]),
-      'terms_acceptance':
-          FormControl<bool>(validators: [Validators.required, Validators.requiredTrue]),
-    });
 FormGroup singIn() => FormGroup({
       'email': FormControl<String>(validators: [Validators.required, Validators.email]),
     });
