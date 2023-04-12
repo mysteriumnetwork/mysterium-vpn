7816b2e29e0d41f321112d5ba438ba1216ad6220
Kristijan <kmitrikjeski@gmail.com>
2023-04-12 16:19:24 +0200

    Use scaffold without safe area for splash page

--- lib/pages/static/splash_page.dart
+++ lib/pages/static/splash_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mysterium_vpn/common/styles/assets.dart';
 import 'package:mysterium_vpn/common/styles/palette.dart';
-import 'package:mysterium_vpn/components/colored_scaffold.dart';
 import 'package:mysterium_vpn/components/svg_icon.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
-  Widget build(BuildContext context) => const ColoredScaffold(
+  Widget build(BuildContext context) => const Scaffold(
         backgroundColor: Palette.darkBlue,
         body: Center(
           child: SvgIcon(
