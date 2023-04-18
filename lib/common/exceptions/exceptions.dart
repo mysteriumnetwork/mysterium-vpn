925eb04fea620603ae87c9d90563a23bae9a718e
Kristijan <kmitrikjeski@gmail.com>
2023-04-18 14:23:17 +0200

    Create incorrect code and magic link exceptions

--- lib/common/exceptions/exceptions.dart
+++ lib/common/exceptions/exceptions.dart
@@ -1,4 +1,6 @@
 export 'api.dart';
+export 'incorrect_code.dart';
+export 'incorrect_magic_link.dart';
 export 'key_does_not_exists.dart';
 export 'package_not_found.dart';
 export 'token_already_used.dart';
