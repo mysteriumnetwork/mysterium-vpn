fc55825711b6686e94aae2ae868d59726ec52a02
Kristijan <kmitrikjeski@gmail.com>
2023-04-18 13:19:39 +0200

    Create PKCE (Proof Key for Code Exchange) model
    Generating code verifier and code challenge pair

---
+++ lib/models/pkce.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:crypto/crypto.dart';
+
+/// A pair of ([codeVerifier], [codeChallenge]) that can be used with PKCE
+/// (Proof Key for Code Exchange).
+class PkcePair {
+  /// Generates a [PkcePair].
+  ///
+  /// [length] is the length used to generate the [codeVerifier].
+  factory PkcePair.generate({int length = 128}) {
+    final random = Random.secure();
+    final verifier = base64UrlEncode(List.generate(length, (_) => random.nextInt(256)));
+    final hash = sha256.convert(ascii.encode(verifier));
+
+    final challenge =
+        base64Url.encode(hash.bytes).replaceAll('=', '').replaceAll('+', '-').replaceAll('/', '_');
+
+    return PkcePair._(verifier, challenge);
+  }
+
+  const PkcePair._(this.codeVerifier, this.codeChallenge);
+
+  /// The code verifier.
+  final String codeVerifier;
+
+  /// The code challenge, computed as base64Url(sha256([codeVerifier])) with
+  /// padding removed as per the spec.
+  final String codeChallenge;
+}
