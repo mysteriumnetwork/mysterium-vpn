392d7704e8fc95257bdf0d80717add75e9496aeb
Kristijan <kmitrikjeski@gmail.com>
2023-04-24 11:05:44 +0200

    Add platform product ids to plan details

--- lib/models/plan_details.dart
+++ lib/models/plan_details.dart
@@ -11,6 +11,8 @@ class PlanDetails with _$PlanDetails {
     @JsonKey(name: 'interval') required Interval enabled,
     @JsonKey(name: 'price') required Price price,
     @JsonKey(name: 'supported_gateways') required List<String> supportedGateways,
+    @JsonKey(name: 'apple_product_id') required String appleProductId,
+    @JsonKey(name: 'google_product_id') required String googleProductId,
   }) = _PlanDetails;
 
   factory PlanDetails.fromJson(Map<String, dynamic> json) => _$PlanDetailsFromJson(json);
