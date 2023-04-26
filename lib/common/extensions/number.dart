ac328d7537e0ea0c3e92beb9758f1274e3328722
Kristijan <kmitrikjeski@gmail.com>
2023-04-26 10:51:59 +0200

    Replace $ sign with currency symbol and currency code

--- lib/common/extensions/number.dart
+++ lib/common/extensions/number.dart
@@ -1,5 +1,14 @@
 extension NumberExtensions on double {
-  String pricePerMonth({required int months}) => '\$${(this / months).toStringAsFixed(2)}';
+  String pricePerMonth({
+    required int months,
+    required String currencySymbol,
+    required String currencyCode,
+  }) =>
+      '${currencySymbol.isEmpty ? currencyCode : currencySymbol}${(this / months).toStringAsFixed(2)}';
 
-  String price() => '\$${toStringAsFixed(2)}';
+  String price({
+    required String currencySymbol,
+    required String currencyCode,
+  }) =>
+      '${currencySymbol.isEmpty ? currencyCode : currencySymbol}${toStringAsFixed(2)}';
 }
