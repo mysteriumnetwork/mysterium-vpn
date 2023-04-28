5d8ef57dc3187948cf36f9914ec4855a1e6cf043
Kristijan <kmitrikjeski@gmail.com>
2023-04-28 16:49:44 +0200

    Disconnect from VPN when app is detached

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:beamer/beamer.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mobx/mobx.dart';
@@ -15,13 +16,29 @@ import 'package:mysterium_vpn/providers/state_providers.dart';
 import 'package:mysterium_vpn/views/home/home_desktop_view.dart';
 import 'package:mysterium_vpn/views/home/home_mobile_view.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final subscriptionStore = ref.watch(subscriptionStorePOD);
     final authStore = ref.watch(authStorePOD);
+    final appLifecycleState = useAppLifecycleState();
+    final vpnStore = ref.watch(vpnStorePOD);
+
+    useEffect(
+      () {
+        debugPrint(appLifecycleState?.name);
+        if (appLifecycleState == AppLifecycleState.resumed) {
+          subscriptionStore.fetchSubscription();
+        }
+        if (appLifecycleState == AppLifecycleState.detached) {
+          vpnStore.disconnect();
+        }
+        return null;
+      },
+      [appLifecycleState],
+    );
     return ReactionBuilder(
       builder: (context) => reaction((_) => subscriptionStore.subscriptionFuture?.status, (result) {
         debugPrint(result.toString());
