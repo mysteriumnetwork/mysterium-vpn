f4863ac8b25fc667a61f9c9b8f06226d5ace8271
Kristijan <kmitrikjeski@gmail.com>
2023-05-16 12:03:27 +0200

    Remove auth store from base app bar

--- lib/components/base_app_bar.dart
+++ lib/components/base_app_bar.dart
@@ -3,25 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:mysterium_vpn/common/styles/assets.dart';
 import 'package:mysterium_vpn/components/app_logo.dart';
 import 'package:mysterium_vpn/components/svg_icon_button.dart';
-import 'package:mysterium_vpn/stores/auth_store.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 class BaseAppBar extends StatelessWidget {
   const BaseAppBar({
-    required this.authStore,
     this.onBackButtonPressed,
     super.key,
   });
-  final AuthStore authStore;
   final VoidCallback? onBackButtonPressed;
   @override
   Widget build(BuildContext context) => Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           SvgIconButton(
-            onPressed: () {
-              Beamer.of(context).popRoute();
-            },
+            onPressed: onBackButtonPressed ??
+                () {
+                  Beamer.of(context).popRoute();
+                },
             asset: Assets.navigateBack,
           ),
           const AppLogo(),
