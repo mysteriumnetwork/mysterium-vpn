b28ff0a2aac606e67088efda7338296d31baa20a
Kristijan <kmitrikjeski@gmail.com>
2023-05-18 15:36:39 +0200

    Fix problem with unknown user

--- lib/stores/subscription_store.dart
+++ lib/stores/subscription_store.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:mobx/mobx.dart';
@@ -191,12 +192,13 @@ abstract class _SubscriptionStore with Store {
 
   @action
   Future<void> _handlePurchase(PurchaseDetails purchaseDetails) async {
-    final index = _products.indexWhere((element) => element.id == _purchasedProductId);
+    final product = _products
+        .firstWhereOrNull((element) => element.productDetails.id == purchaseDetails.productID);
 
     if (purchaseDetails.status == PurchaseStatus.error ||
         purchaseDetails.status == PurchaseStatus.canceled) {
-      if (index != -1) {
-        _products[index].status = ProductStatus.purchasable;
+      if (product != null) {
+        product.status = ProductStatus.purchasable;
       }
       if (purchaseDetails.status == PurchaseStatus.canceled) {
         _subscriptionService.clearPendingTransactions();
@@ -206,15 +208,15 @@ abstract class _SubscriptionStore with Store {
     }
 
     if (purchaseDetails.status == PurchaseStatus.pending) {
-      if (index != -1) {
-        _products[index].status = ProductStatus.pending;
+      if (product != null) {
+        product.status = ProductStatus.pending;
       }
       return;
     }
     _subscription = await verifyPurchase(_purchasedProductId ?? '', purchaseDetails);
 
     if (purchaseDetails.status == PurchaseStatus.purchased && (_subscription?.active ?? false)) {
-      if (index != -1) {
+      if (product != null) {
         for (final product in _products) {
           product.status = product.planDetails.id == _purchasedProductId
               ? ProductStatus.purchased
@@ -222,7 +224,7 @@ abstract class _SubscriptionStore with Store {
         }
         _analyticsStore.setPaymentSuccessful(
           paymentGateway: getPlatformGateway(),
-          planPrice: _products[index].productDetails.rawPrice,
+          planPrice: product.productDetails.rawPrice,
           planType: _purchasedProductId ?? '',
           transactionId: purchaseDetails.verificationData.serverVerificationData,
           transactionDate: purchaseDetails.transactionDate ?? '',
