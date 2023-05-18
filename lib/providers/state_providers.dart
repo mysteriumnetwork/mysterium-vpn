68dc23cbaf2ba74a8bcc105778dd61b81f3efec0
Kristijan <kmitrikjeski@gmail.com>
2023-05-18 11:24:11 +0200

    Move router delegate to separate file

--- lib/providers/state_providers.dart
+++ lib/providers/state_providers.dart
@@ -2,13 +2,8 @@
 
 import 'dart:async';
 
-import 'package:beamer/beamer.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mobx/mobx.dart';
-import 'package:mysterium_vpn/common/enums/enums.dart';
-import 'package:mysterium_vpn/common/extensions/enum.dart';
-import 'package:mysterium_vpn/common/router/router.dart';
 import 'package:mysterium_vpn/models/flavor_config.dart';
 import 'package:mysterium_vpn/providers/service_providers.dart';
 import 'package:mysterium_vpn/stores/analytics_store.dart';
@@ -21,7 +16,6 @@ import 'package:mysterium_vpn/stores/rest_store.dart';
 import 'package:mysterium_vpn/stores/subscription_store.dart';
 import 'package:mysterium_vpn/stores/theme_store.dart';
 import 'package:mysterium_vpn/stores/vpn_store.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 
 final localeStorePOD = Provider<LocaleStore>((ref) => LocaleStore());
 
@@ -84,52 +78,6 @@ final subscriptionStorePOD = Provider<SubscriptionStore>((ref) {
 final restApiStorePOD = Provider<RestStore>((ref) {
   final apiService = ref.read(apiServicePOD);
   return RestStore(apiService: apiService);
-});
-
-final routeInformationParserPOD = Provider((ref) => BeamerParser());
-
-final routerDelegatePOD = Provider<BeamerDelegate>((ref) {
-  final authStore = ref.read(authStorePOD);
-  final firebaseAnalytics = ref.read(firebaseAnalyticsPOD);
-  return BeamerDelegate(
-    navigatorObservers: [
-      FirebaseAnalyticsObserver(
-        analytics: firebaseAnalytics,
-        nameExtractor: (settings) => settings.name,
-      ),
-      SentryNavigatorObserver(),
-    ],
-    guards: [
-      BeamGuard(
-        pathPatterns: [
-          Routes.home.toRoute,
-          Routes.settings.toRoute,
-          Routes.subscription.toRoute,
-          Routes.emailCommunications.toRoute,
-          Routes.notifications.toRoute,
-          Routes.vpnConfigConsent.toRoute,
-        ],
-        check: (context, state) => authStore.authStatus == AuthStatus.authenticated,
-        beamToNamed: (_, __) => Routes.login.toRoute,
-      ),
-      BeamGuard(
-        pathPatterns: [Routes.login.toRoute],
-        check: (context, state) =>
-            authStore.authStatus == AuthStatus.unauthenticated ||
-            authStore.authStatus == AuthStatus.authenticating,
-        beamToNamed: (_, __) => Routes.home.toRoute,
-      ),
-      BeamGuard(
-        pathPatterns: [Routes.splash.toRoute],
-        check: (context, state) => authStore.authStatus == AuthStatus.unknown,
-        beamToNamed: (_, __) => authStore.authStatus == AuthStatus.authenticated
-            ? Routes.home.toRoute
-            : Routes.login.toRoute,
-      ),
-    ],
-    initialPath: Routes.splash.toRoute,
-    locationBuilder: (routeInformation, _) => BeamerLocations(routeInformation),
-  );
 });
 
 final environmentPOD = StateProvider<FlavorConfig>(
