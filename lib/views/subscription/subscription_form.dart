bdd349689d04735bbdf9c0001b8cf55b7e912cad
Kristijan <kmitrikjeski@gmail.com>
2023-05-26 09:50:15 +0200

    Implement subscription verifying dialog

--- lib/views/subscription/subscription_form.dart
+++ lib/views/subscription/subscription_form.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:mobx/mobx.dart';
 import 'package:mysterium_vpn/common/enums/enums.dart';
 import 'package:mysterium_vpn/common/extensions/enum.dart';
@@ -16,6 +15,7 @@ import 'package:mysterium_vpn/components/easy_button.dart';
 import 'package:mysterium_vpn/components/easy_text.dart';
 import 'package:mysterium_vpn/components/error_widget.dart';
 import 'package:mysterium_vpn/components/header_title.dart';
+import 'package:mysterium_vpn/components/loading_barrier.dart';
 import 'package:mysterium_vpn/components/loading_indicator.dart';
 import 'package:mysterium_vpn/generated/locale_keys.g.dart';
 import 'package:mysterium_vpn/services/local_db_service.dart';
@@ -44,105 +44,131 @@ class SubscriptionForm extends HookConsumerWidget {
       ),
       [store.subscription?.active],
     );
-    return Column(
-      children: [
-        HeaderTitle(
-          text: LocaleKeys.selectPackage.tr(),
-        ),
-        Observer(
-          builder: (context) {
-            if (store.products.isEmpty) {
-              return RetryOnErrorWidget(
-                error: LocaleKeys.productsNotAvailable.tr(),
-                onRetry: store.getSubscriptionsConfig,
-              );
-            }
-            return Column(
-              children: [
-                SubscriptionProductsList(
-                  products: store.products,
-                  originalPrice: store.originalPrice,
-                ).padding(bottom: getMediaHeight(context) * 0.02),
-                EasyText(
-                  subsFormStatus == SubscriptionFormStatus.manage
-                      ? LocaleKeys.manageSubsTittle.tr()
-                      : subsFormStatus == SubscriptionFormStatus.expired
-                          ? LocaleKeys.subsExpiredTittle.tr()
-                          : LocaleKeys.freeTrialTitle.tr(),
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                ).padding(bottom: getMediaHeight(context) * 0.005),
-                EasyText(
-                  subsFormStatus == SubscriptionFormStatus.manage
-                      ? LocaleKeys.manageSubsDesc.tr()
-                      : subsFormStatus == SubscriptionFormStatus.expired
-                          ? LocaleKeys.subsExpiredDesc.tr()
-                          : LocaleKeys.freeTrialDesc.tr(),
-                  maxLines: 3,
-                  textAlign: TextAlign.center,
-                ).padding(bottom: getMediaHeight(context) * 0.025),
-                ReactionBuilder(
-                  builder: (context) => reaction((_) => store.purchaseStatus, (result) {
-                    if (result == PurchaseStatus.purchased && isMounted()) {
-                      if (store.subscription?.active ?? false) {
-                        showSnackbar(
-                          LocaleKeys.subscriptionActive.tr(),
-                          type: MessageType.success,
-                        );
-                        context.beamToReplacementNamed(Routes.home.toRoute);
-                      } else {
-                        shownRetryDialog(
-                          onRetry: () async => store.retryVerificationProcess(),
-                          context: context,
-                          asset: Assets.subscription,
-                          title: LocaleKeys.subscriptionVerificationFailed.tr(),
-                          subtitle: LocaleKeys.failedToVerifySubs.tr(),
-                        );
-                      }
-                    }
-                    if (result == PurchaseStatus.canceled) {
-                      showSnackbar(LocaleKeys.subscriptionProcessCanceled.tr());
-                    }
-                    if (result == PurchaseStatus.error) {
-                      showSnackbar(
-                        LocaleKeys.failedToSubscribe.tr(),
-                      );
-                    }
-                  }),
-                  child: EasyButton(
-                    width: getMediaWidth(context) * 0.8,
-                    useSystemColor: false,
-                    color: store.purchaseStatus == PurchaseStatus.pending
-                        ? Theme.of(context).disabledColor
-                        : Palette.purple,
-                    onPressed: store.purchaseStatus == PurchaseStatus.pending
-                        ? null
-                        : () async {
-                            store.subscribeToPackage();
-                          },
-                    child: store.purchaseStatus == PurchaseStatus.pending
-                        ? const LoadingIndicator(
-                            radius: 20,
-                            strokeWidth: 1.5,
-                          )
-                        : EasyText(
-                            subsFormStatus == SubscriptionFormStatus.manage
-                                ? store.selectedProductId == store.purchasedProductId
-                                    ? LocaleKeys.manageBtn.tr()
-                                    : LocaleKeys.changeSubPlan.tr()
-                                : subsFormStatus == SubscriptionFormStatus.expired
-                                    ? LocaleKeys.renewSubsBtn.tr()
-                                    : LocaleKeys.startTrialBtn.tr(),
-                            color: Palette.white,
-                          ),
-                  ),
+
+    return Observer(
+      builder: (context) => Stack(
+        children: [
+          Column(
+            children: [
+              HeaderTitle(
+                text: LocaleKeys.selectPackage.tr(),
+              ),
+              if (store.products.isEmpty)
+                RetryOnErrorWidget(
+                  error: LocaleKeys.productsNotAvailable.tr(),
+                  onRetry: store.getSubscriptionsConfig,
+                )
+              else
+                Column(
+                  children: [
+                    SubscriptionProductsList(
+                      products: store.products,
+                      originalPrice: store.originalPrice,
+                    ).padding(bottom: getMediaHeight(context) * 0.02),
+                    EasyText(
+                      subsFormStatus == SubscriptionFormStatus.manage
+                          ? LocaleKeys.manageSubsTittle.tr()
+                          : subsFormStatus == SubscriptionFormStatus.expired
+                              ? LocaleKeys.subsExpiredTittle.tr()
+                              : LocaleKeys.freeTrialTitle.tr(),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ).padding(bottom: getMediaHeight(context) * 0.005),
+                    EasyText(
+                      subsFormStatus == SubscriptionFormStatus.manage
+                          ? LocaleKeys.manageSubsDesc.tr()
+                          : subsFormStatus == SubscriptionFormStatus.expired
+                              ? LocaleKeys.subsExpiredDesc.tr()
+                              : LocaleKeys.freeTrialDesc.tr(),
+                      maxLines: 3,
+                      textAlign: TextAlign.center,
+                    ).padding(bottom: getMediaHeight(context) * 0.025),
+                    ReactionBuilder(
+                      builder: (context) => reaction((_) => store.subscriptonStatus, (status) {
+                        if (isMounted()) {
+                          if (status == SubscriptionStatus.purchased) {
+                            showSnackbar(
+                              LocaleKeys.subscriptionActive.tr(),
+                              type: MessageType.success,
+                            );
+                            context.beamToReplacementNamed(Routes.home.toRoute);
+                          } else if (status == SubscriptionStatus.notVerified ||
+                              status == SubscriptionStatus.verifyingError) {
+                            shownRetryDialog(
+                              onRetry: () async => store.retryVerificationProcess(),
+                              context: context,
+                              asset: Assets.subscription,
+                              title: LocaleKeys.subscriptionVerificationFailed.tr(),
+                              subtitle: LocaleKeys.failedToVerifySubs.tr(),
+                            );
+                          }
+                        }
+
+                        if (status == SubscriptionStatus.canceled) {
+                          showSnackbar(LocaleKeys.subscriptionProcessCanceled.tr());
+                        }
+                        if (status == SubscriptionStatus.error) {
+                          showSnackbar(
+                            LocaleKeys.failedToSubscribe.tr(),
+                          );
+                        }
+                      }),
+                      child: EasyButton(
+                        width: getMediaWidth(context) * 0.8,
+                        useSystemColor: false,
+                        color: store.subscriptonStatus == SubscriptionStatus.pending ||
+                                store.subscriptonStatus == SubscriptionStatus.verifying
+                            ? Theme.of(context).disabledColor
+                            : Palette.purple,
+                        onPressed: store.subscriptonStatus == SubscriptionStatus.pending ||
+                                store.subscriptonStatus == SubscriptionStatus.verifying
+                            ? null
+                            : () async {
+                                store.subscribeToPackage();
+                              },
+                        child: store.subscriptonStatus == SubscriptionStatus.pending ||
+                                store.subscriptonStatus == SubscriptionStatus.verifying
+                            ? const LoadingIndicator(
+                                radius: 20,
+                                strokeWidth: 1.5,
+                              )
+                            : EasyText(
+                                subsFormStatus == SubscriptionFormStatus.manage
+                                    ? store.selectedProductId == store.purchasedProductId
+                                        ? LocaleKeys.manageBtn.tr()
+                                        : LocaleKeys.changeSubPlan.tr()
+                                    : subsFormStatus == SubscriptionFormStatus.expired
+                                        ? LocaleKeys.renewSubsBtn.tr()
+                                        : LocaleKeys.startTrialBtn.tr(),
+                                color: Palette.white,
+                              ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            );
-          },
-        ),
-      ],
-    ).scrollable().padding(horizontal: 20);
+            ],
+          ).scrollable().padding(horizontal: 20),
+          if (store.subscriptonStatus == SubscriptionStatus.verifying)
+            LoadingBarrier(
+              color: Palette.darkBlue,
+              child: Center(
+                child: LoadingIndicator(
+                  radius: 50,
+                  strokeWidth: 3,
+                  message: LocaleKeys.processingPayment.tr(),
+                  messageColor: Palette.black,
+                )
+                    .decorated(
+                      color: Palette.white,
+                      borderRadius: BorderRadius.circular(10),
+                    )
+                    .padding(all: 20)
+                    .constrained(width: getMediaWidth(context) * 0.8, height: 200),
+              ),
+            )
+        ],
+      ),
+    );
   }
 }
 
