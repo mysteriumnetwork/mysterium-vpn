734f899405ec28eac00fdae9d18f4598fed2240c
Kristijan <58517527+KristijanMitrik@users.noreply.github.com>
2023-06-07 16:00:14 +0200

    Remove `testEmail` from flavor config

--- lib/models/flavor_config.dart
+++ lib/models/flavor_config.dart
@@ -6,7 +6,6 @@ class FlavorValues {
     required this.scheme,
     required this.webAppUrl,
     required this.sentryDsn,
-    required this.testEmail,
     required this.billingPage,
   });
 
@@ -16,7 +15,6 @@ class FlavorValues {
         webAppUrl: 'app.mysteriumvpn.com',
         sentryDsn:
             'https://62d0b0c708d8492ca4921472bd99ebec@o136129.ingest.sentry.io/4504949838643200',
-        testEmail: 'Yet7ej38fcBf3pzrE6xK.S3xFjgNn8rRzrkmLoag8@mysteriumvpn.com',
         billingPage: 'https://app.mysteriumvpn.com/dashboard/billing',
       );
   factory FlavorValues.dev() => FlavorValues(
@@ -25,7 +23,6 @@ class FlavorValues {
         webAppUrl: 'app-testnet.mysteriumvpn.com',
         sentryDsn:
             'https://62d0b0c708d8492ca4921472bd99ebec@o136129.ingest.sentry.io/4504949838643200',
-        testEmail: 'ttcdELfiaq8sgA4D6Y8A.iMC9Y5QyDLrxJnr5cXXC@mysteriumvpn.com',
         billingPage: 'https://app-testnet.mysteriumvpn.com/dashboard/billing',
       );
 
@@ -33,7 +30,6 @@ class FlavorValues {
   final String scheme;
   final String webAppUrl;
   final String sentryDsn;
-  final String testEmail;
   final String billingPage;
 }
 
