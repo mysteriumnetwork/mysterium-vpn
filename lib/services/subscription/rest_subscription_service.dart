135c680044b120f9b566ec486626e4af2cd338ec
Kristijan <58517527+KristijanMitrik@users.noreply.github.com>
2023-06-15 22:41:01 +0200

    Remove redundant purchaseId

--- lib/services/subscription/rest_subscription_service.dart
+++ lib/services/subscription/rest_subscription_service.dart
@@ -86,14 +86,13 @@ class RestSubscriptionService extends SubscriptionService {
   }) async {
     late PurchaseParam purchaseParam;
     if (Platform.isAndroid) {
-      final purchaseId = _localDb.getSubscriptionPurchaseId();
       GooglePlayPurchaseDetails? details;
-      if (purchasedProductId != null && purchaseId != null) {
+      if (purchasedProductId != null) {
         final androidAddition =
             _inAppPurchase.getPlatformAddition<InAppPurchaseAndroidPlatformAddition>();
         final oldPurchases = await androidAddition.queryPastPurchases();
         final oldPurchase = oldPurchases.pastPurchases.where(
-          (element) => element.productID == purchasedProductId && element.transactionDate !=null,
+          (element) => element.productID == purchasedProductId && element.transactionDate != null,
         );
         if (oldPurchase.isNotEmpty) {
           details = oldPurchase.sortedBy((e) => e.transactionDate!).last;
