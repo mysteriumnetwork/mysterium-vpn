5f58f607848ac23b7de5150d7c1b82001e1bba78
Kristijan <58517527+KristijanMitrik@users.noreply.github.com>
2023-06-20 17:47:49 +0200

    Implement app lifecycle listener for desktop

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,9 +14,11 @@ import flutter_secure_storage_macos
 import in_app_purchase_storekit
 import package_info_plus
 import path_provider_foundation
+import screen_retriever
 import sentry_flutter
 import shared_preferences_foundation
 import url_launcher_macos
+import window_manager
 import wireguard_dart
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -29,8 +31,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
   WireguardDartPlugin.register(with: registry.registrar(forPlugin: "WireguardDartPlugin"))
 }
