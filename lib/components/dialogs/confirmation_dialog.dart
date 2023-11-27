043d3b486d30cb90c79ea6b89971eb5878f44181
Kristijan <kmitrikjeski@gmail.com>
2023-11-27 18:02:23 +0100

    Implement suggest login username on app launch

---
+++ lib/components/dialogs/confirmation_dialog.dart
@@ -0,0 +1,84 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:mysterium_vpn/common/styles/palette.dart';
+import 'package:mysterium_vpn/generated/locale_keys.g.dart';
+
+Future<void> shownConfirmationDialog(
+  BuildContext context, {
+  required String title,
+  required Widget content,
+  required VoidCallback onConfirm,
+  required Widget icon,
+  bool dismissible = true,
+  String? confirmText,
+  String? cancelText,
+}) async {
+  showDialog(
+    context: context,
+    barrierDismissible: dismissible,
+    builder: (context) => _SuggestLoginDialog(
+      title: title,
+      content: content,
+      onConfirm: onConfirm,
+      icon: icon,
+      confirmText: confirmText,
+      cancelText: cancelText,
+    ),
+  );
+}
+
+class _SuggestLoginDialog extends StatelessWidget {
+  const _SuggestLoginDialog({
+    required this.title,
+    required this.content,
+    required this.onConfirm,
+    required this.icon,
+    this.confirmText,
+    this.cancelText,
+  });
+  final String title;
+  final Widget content;
+  final Widget icon;
+  final VoidCallback onConfirm;
+  final String? confirmText;
+  final String? cancelText;
+  @override
+  Widget build(BuildContext context) => SizedBox(
+        width: 200,
+        child: AlertDialog(
+          surfaceTintColor: Palette.white,
+          titlePadding: const EdgeInsets.only(top: 4, left: 16, right: 16),
+          contentPadding: const EdgeInsets.only(top: 4, bottom: 16, left: 16, right: 16),
+          insetPadding: const EdgeInsets.symmetric(horizontal: 15),
+          iconPadding: const EdgeInsets.only(top: 16, bottom: 8),
+          backgroundColor: Palette.white,
+          icon: icon,
+          title: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          actions: [
+            TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Palette.lightBlack),
+              ),
+              child: Text(cancelText ?? LocaleKeys.no.tr()),
+              onPressed: () => Navigator.pop(context),
+            ),
+            TextButton(
+              child: Text(confirmText ?? LocaleKeys.yes.tr()),
+              onPressed: () {
+                Navigator.pop(context);
+                onConfirm();
+              },
+            ),
+          ],
+          content: content,
+        ),
+      );
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,18 +1,74 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mysterium_vpn/common/layout_builders/screen_type_builder.dart';
+import 'package:mysterium_vpn/common/styles/palette.dart';
+import 'package:mysterium_vpn/common/utils/utils.dart';
 import 'package:mysterium_vpn/components/colored_scaffold.dart';
+import 'package:mysterium_vpn/components/dialogs/confirmation_dialog.dart';
+import 'package:mysterium_vpn/generated/locale_keys.g.dart';
+import 'package:mysterium_vpn/providers/state_providers.dart';
+import 'package:mysterium_vpn/stores/auth_store.dart';
 import 'package:mysterium_vpn/views/login/login_desktop_view.dart';
 import 'package:mysterium_vpn/views/login/login_mobile_view.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends HookConsumerWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context) => ColoredScaffold(
-        body: ScreenTypeLayoutBuilder(
-          mobile: (BuildContext context) => const LoginMobileView(),
-          tablet: (BuildContext context) => const LoginDesktopView(),
-          desktop: (BuildContext context) => const LoginDesktopView(),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authStore = ref.watch(authStorePOD);
+    useEffect(
+      () {
+        _suggestLogin(context, authStore);
+        return null;
+      },
+      [],
+    );
+
+    return ColoredScaffold(
+      body: ScreenTypeLayoutBuilder(
+        mobile: (BuildContext context) => const LoginMobileView(),
+        tablet: (BuildContext context) => const LoginDesktopView(),
+        desktop: (BuildContext context) => const LoginDesktopView(),
+      ),
+    );
+  }
+
+  void _suggestLogin(BuildContext context, AuthStore authStore) {
+    if (authStore.temporaryEmail.isEmpty) {
+      return;
+    }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (authStore.temporaryEmail != authStore.email) {
+        authStore.email = authStore.temporaryEmail;
+        handleOnSignIn(context, authStore);
+      } else {
+        _showConfirmationDialog(context, authStore);
+      }
+      authStore.temporaryEmail = '';
+    });
+  }
+
+  Future<void> _showConfirmationDialog(BuildContext context, AuthStore authStore) =>
+      shownConfirmationDialog(
+        context,
+        content: Text(
+          authStore.email,
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+          ),
+          maxLines: 2,
+          textAlign: TextAlign.center,
+        ),
+        title: LocaleKeys.isThisYou.tr(),
+        onConfirm: () => handleOnSignIn(context, authStore),
+        icon: const Icon(
+          Icons.info,
+          color: Palette.black,
+          size: 30,
         ),
       );
 }
