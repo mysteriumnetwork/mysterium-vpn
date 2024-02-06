84376f7e90ef79924bd0076d8b80db3213bfe044
Kristijan <kmitrikjeski@gmail.com>
2024-02-06 14:46:47 +0100

    Add `location` for broken node

--- lib/stores/analytics/analytics_store_firebase.dart
+++ lib/stores/analytics/analytics_store_firebase.dart
@@ -32,18 +32,12 @@ abstract class _AnalyticsStoreFirebase extends AnalyticsStore with Store {
     Object? reason,
     bool fatal = false,
   }) async {
-    if (fatal) {
-      _crashlytics.recordFlutterFatalError(
-        FlutterErrorDetails(exception: err, stack: stack),
-      );
-      return;
-    }
-
     _crashlytics.recordError(
       err,
       stack,
       reason: reason,
       printDetails: true,
+      fatal: fatal,
     );
   }
 
