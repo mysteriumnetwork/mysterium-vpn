c48b5b0cd3825ac92b31c7b2d0248fa65ab85085
Kristijan <kmitrikjeski@gmail.com>
2024-04-04 15:38:21 +0200

    Upgrade `flutter_native_splash`

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
-import 'dart:async';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mysterium_vpn/entrypoints/enviroment.dart';
 import 'package:mysterium_vpn/entrypoints/firebase/firebase_options_dev.dart' as dev;
 import 'package:mysterium_vpn/entrypoints/firebase/firebase_options_prod.dart' as prod;
 import 'package:mysterium_vpn/models/flavor_config.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   const flavor = String.fromEnvironment('FLAVOR');
-  runZonedGuarded(() {
-    Enviroment().launch(
-      flavor: flavor,
-      firebaseOptions: _getFirebaseOptions(flavor),
-    );
-  }, (error, stackTrace) async {
-    await SentryFlutter.init((options) {
-      options.dsn =
-          'https://62d0b0c708d8492ca4921472bd99ebec@o136129.ingest.sentry.io/4504949838643200';
-    });
-    await Sentry.captureException(error, stackTrace: stackTrace);
-  });
+  Enviroment().launch(
+    flavor: flavor,
+    firebaseOptions: _getFirebaseOptions(flavor),
+  );
 }
 
 FirebaseOptions? _getFirebaseOptions(String flavor) {
