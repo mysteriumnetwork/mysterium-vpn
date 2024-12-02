505fdcbee4b14403a3c3519d3ef2612ab00a7868
Kristijan <kmitrikjeski@gmail.com>
2024-12-02 19:11:38 +0100

    Remove unused pages

--- lib/common/forms/forms.dart
+++ lib/common/forms/forms.dart
@@ -3,9 +3,6 @@ import 'package:reactive_forms/reactive_forms.dart';
 FormGroup singIn() => FormGroup({
       'email': FormControl<String>(validators: [Validators.required, Validators.email]),
     });
-FormGroup reportIssue() => FormGroup({
-      'report_issue': FormControl<String>(validators: [Validators.required]),
-    });
 
 FormGroup marketingConsent() => FormGroup({
       'consent': FormControl<bool>(value: true),
