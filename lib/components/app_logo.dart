c56ddc3b728545350d9e7fa3ccdbef60575305c3
dmacan <david.macan@zeromolecule.com>
2024-12-10 13:40:22 +0100

    fix: app logo color config

--- lib/components/app_logo.dart
+++ lib/components/app_logo.dart
@@ -1,27 +1,24 @@
-// Flutter imports:
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mysterium_vpn/common/styles/assets.dart';
-import 'package:mysterium_vpn/common/utils/utils.dart';
 import 'package:mysterium_vpn/components/svg_icon.dart';
-import 'package:mysterium_vpn/providers/state_providers.dart';
-// Project imports:
 
-class AppLogo extends ConsumerWidget {
-  const AppLogo({super.key});
+class AppLogo extends StatelessWidget {
+  const AppLogo({
+    super.key,
+    this.brightness,
+  });
+
+  final Brightness? brightness;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final themeStore = ref.read(themeStorePOD);
-    return Observer(
-      builder: (context) {
-        final asset = themeStore.isDarkMode || checkMediaWidth(context, 750)
-            ? Assets.logoWhiteSvg
-            : Assets.logoBlackSvg;
-        return SvgIcon(
-          asset: asset,
-        );
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final brightness = this.brightness ?? theme.brightness;
+
+    return SvgIcon(
+      asset: switch (brightness) {
+        Brightness.dark => Assets.logoWhiteSvg,
+        Brightness.light => Assets.logoBlackSvg,
       },
     );
   }
