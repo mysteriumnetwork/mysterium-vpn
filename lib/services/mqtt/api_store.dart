8620474a520b2d70f9cc4b939896678a81bd66db
Valdas Petrulis <petrulis.valdas@gmail.com>
2024-12-19 07:58:51 +0200

    Use separate DTO for healthcheck

--- lib/services/mqtt/api_store.dart
+++ lib/services/mqtt/api_store.dart
@@ -31,7 +31,7 @@ abstract class _ApiStore with Store {
   StreamSubscription<String>? _healthcheckSub;
 
   @readonly
-  HealthcheckResponse? _lastHealthcheck;
+  HealthcheckMessage? _lastHealthcheck;
 
   Future<void> initStore() async {
     try {
@@ -41,7 +41,7 @@ abstract class _ApiStore with Store {
 
       await _mqtt.ensureStart();
       _healthcheckSub ??= _mqtt.subscribe('healthcheck').listen((event) {
-        _lastHealthcheck = HealthcheckResponse.fromJson(json.decode(event) as Map<String, dynamic>);
+        _lastHealthcheck = HealthcheckMessage.fromJson(json.decode(event) as Map<String, dynamic>);
       });
     } catch (e, stackTrace) {
       _logger.handle(e, stackTrace);
