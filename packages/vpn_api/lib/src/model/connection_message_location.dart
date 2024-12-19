91fd142e4c326ece04c0dffa789f36e91f465653
Valdas Petrulis <petrulis.valdas@gmail.com>
2024-12-19 07:57:28 +0200

    Bump VPNApi client

---
+++ packages/vpn_api/lib/src/model/connection_message_location.dart
@@ -0,0 +1,54 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+
+// ignore_for_file: unused_element
+import 'package:json_annotation/json_annotation.dart';
+
+part 'connection_message_location.g.dart';
+
+@JsonSerializable(
+  checked: true,
+  createToJson: true,
+  disallowUnrecognizedKeys: false,
+  explicitToJson: true,
+)
+class ConnectionMessageLocation {
+  /// Returns a new [ConnectionMessageLocation] instance.
+  ConnectionMessageLocation({
+    required this.ip,
+    required this.country,
+  });
+
+  @JsonKey(
+    name: r'ip',
+    required: true,
+    includeIfNull: false,
+  )
+  final String ip;
+
+  @JsonKey(
+    name: r'country',
+    required: true,
+    includeIfNull: false,
+  )
+  final String country;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ConnectionMessageLocation && other.ip == ip && other.country == country;
+
+  @override
+  int get hashCode => ip.hashCode + country.hashCode;
+
+  factory ConnectionMessageLocation.fromJson(Map<String, dynamic> json) =>
+      _$ConnectionMessageLocationFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ConnectionMessageLocationToJson(this);
+
+  @override
+  String toString() {
+    return toJson().toString();
+  }
+}
