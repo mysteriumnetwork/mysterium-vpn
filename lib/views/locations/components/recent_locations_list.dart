ddc4ea2c82ba446901e08c9f1d0dc3b9cfe788ce
dmacan <david.macan@zeromolecule.com>
2025-01-17 12:29:31 +0100

    fix: recent locations display, increased initial sheet height by 5%

--- lib/views/locations/components/recent_locations_list.dart
+++ lib/views/locations/components/recent_locations_list.dart
@@ -6,7 +6,7 @@ class RecentLocationsList extends StatelessWidget {
   const RecentLocationsList({
     required this.items,
     required this.onItemPressed,
-    this.constraints = const BoxConstraints(maxHeight: 146),
+    this.constraints = const BoxConstraints(maxHeight: 132),
     super.key,
   });
 
