31df321fa93a5f291c0976c9c4ce619601818864
dmacan <david.macan@zeromolecule.com>
2025-01-27 10:11:30 +0100

    refactor: initialize Intercom on first use
    Refactors Intercom initialization to happen on the first use instead of on app startup.
    This is achieved by using `ObservableFuture` to trigger the initialization when any Intercom function is called for the first time.
    Removes the `initialize` method from `IntercomStore` and its implementations, as it's no longer needed.

--- lib/stores/intercom/intercom_desktop_store.g.dart
+++ lib/stores/intercom/intercom_desktop_store.g.dart
@@ -9,14 +9,6 @@ part of 'intercom_desktop_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$IntercomDesktopStore on _IntercomDesktopStore, Store {
-  late final _$initializeAsyncAction =
-      AsyncAction('_IntercomDesktopStore.initialize', context: context);
-
-  @override
-  Future<void> initialize() {
-    return _$initializeAsyncAction.run(() => super.initialize());
-  }
-
   late final _$registerUserAsyncAction =
       AsyncAction('_IntercomDesktopStore.registerUser', context: context);
 
