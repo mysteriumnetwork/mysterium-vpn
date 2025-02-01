dca08906e8adf4d953b106c6dfafd0d4b45e6421
Valdas Petrulis <petrulis.valdas@gmail.com>
2025-02-01 14:24:03 +0100

    Bump VPNApi client

--- packages/vpn_api/lib/src/model/magic_link_redirect_request_query.g.dart
+++ packages/vpn_api/lib/src/model/magic_link_redirect_request_query.g.dart
@@ -18,10 +18,11 @@ MagicLinkRedirectRequestQuery _$MagicLinkRedirectRequestQueryFromJson(Map<String
         final val = MagicLinkRedirectRequestQuery(
           code: $checkedConvert('code', (v) => v as String),
           continueTo: $checkedConvert('continue_to', (v) => v as String?),
+          redirectUrl: $checkedConvert('redirect_url', (v) => v as String?),
         );
         return val;
       },
-      fieldKeyMap: const {'continueTo': 'continue_to'},
+      fieldKeyMap: const {'continueTo': 'continue_to', 'redirectUrl': 'redirect_url'},
     );
 
 Map<String, dynamic> _$MagicLinkRedirectRequestQueryToJson(MagicLinkRedirectRequestQuery instance) {
@@ -36,5 +37,6 @@ Map<String, dynamic> _$MagicLinkRedirectRequestQueryToJson(MagicLinkRedirectRequ
   }
 
   writeNotNull('continue_to', instance.continueTo);
+  writeNotNull('redirect_url', instance.redirectUrl);
   return val;
 }
