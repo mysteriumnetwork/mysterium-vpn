9ee01a6ce5323c24f46dafcda0105f8752ad8f63
dmacan <david.macan@zeromolecule.com>
2025-02-05 15:32:15 +0100

    feat: introduce `useABTest` hook

---
+++ lib/common/hooks/ab_test_hook.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:mysterium_vpn/common/hooks/hooks.dart';
+import 'package:mysterium_vpn/providers/state_providers.dart';
+import 'package:mysterium_vpn/stores/remote_config/ab_testing_store.dart';
+
+String useABTest(String Function(ABTestingStore store) selector) {
+  final store = useProvider(abTestingStorePOD);
+  final selectorRef = useRef(selector)..value = selector;
+  return useComputedValue(() => selectorRef.value(store), [selectorRef]);
+}
