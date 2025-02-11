95c96a8a01f40cc8b340bf769e9a63e47baa51b3
dmacan <david.macan@zeromolecule.com>
2025-02-11 10:33:05 +0100

    feat: add unit tests for `NumberExtensions` methods

---
+++ test/extensions/number_extensions_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mysterium_vpn/common/extensions/extensions.dart';
+
+void main() {
+  group('NumberExtensions', () {
+    test('pricePerMonth returns correct value with currency symbol', () {
+      const amount = 120.0;
+      final result = amount.pricePerMonth(
+        months: 12,
+        currencySymbol: r'$',
+        currencyCode: 'USD',
+      );
+      expect(result, r'$10.00');
+    });
+
+    test('pricePerMonth returns correct value with currency code', () {
+      const amount = 120.0;
+      final result = amount.pricePerMonth(
+        months: 12,
+        currencySymbol: '',
+        currencyCode: 'USD',
+      );
+      expect(result, 'USD10.00');
+    });
+
+    test('price returns correct value with currency symbol', () {
+      const amount = 120.0;
+      final result = amount.price(
+        currencySymbol: r'$',
+        currencyCode: 'USD',
+      );
+      expect(result, r'$120.00');
+    });
+
+    test('price returns correct value with currency code', () {
+      const amount = 120.0;
+      final result = amount.price(
+        currencySymbol: '',
+        currencyCode: 'USD',
+      );
+      expect(result, 'USD120.00');
+    });
+  });
+}
