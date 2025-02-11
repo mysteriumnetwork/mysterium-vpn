7534967655785abf246f2c9f2790145f6efa8df8
dmacan <david.macan@zeromolecule.com>
2025-02-11 16:23:29 +0100

    feat: add banners store unit tests

--- test/stores/banners_store_test.dart
+++ test/stores/banners_store_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:mysterium_vpn/common/enums/banner_type.dart';
 import 'package:mysterium_vpn/services/api/api_service.dart';
@@ -6,12 +7,13 @@ import 'package:mysterium_vpn/stores/banners_store.dart';
 import 'package:mysterium_vpn/stores/remote_config/remote_config_store.dart';
 import 'package:mysterium_vpn/stores/subscription_store.dart';
 
-class MockApiService extends Mock implements ApiService {}
+import 'banners_store_test.mocks.dart';
 
-class MockSubscriptionStore extends Mock implements SubscriptionStore {}
-
-class MockRemoteConfigStore extends Mock implements RemoteConfigStore {}
-
+@GenerateNiceMocks([
+  MockSpec<ApiService>(),
+  MockSpec<SubscriptionStore>(),
+  MockSpec<RemoteConfigStore>(),
+])
 void main() {
   group('BannersStore', () {
     late BannersStore bannersStore;
@@ -24,45 +26,48 @@ void main() {
       mockSubscriptionStore = MockSubscriptionStore();
       mockRemoteConfigStore = MockRemoteConfigStore();
 
-      when(mockApiService.getShownBanners()).thenAnswer((_) async => []);
-
       bannersStore = BannersStore(mockApiService, mockSubscriptionStore, mockRemoteConfigStore);
-      await bannersStore.shownBanners;
     });
 
     group('banners', () {
       test('returns all banners when no banners are shown and not subscribed', () async {
         when(mockApiService.getShownBanners()).thenAnswer((_) async => <BannerType>[]);
-        await Future.delayed(Duration.zero); // Allow MobX to react
-
         when(mockSubscriptionStore.isSubscribed).thenReturn(false);
-        when(mockRemoteConfigStore.dataCenterCountries).thenReturn([]);
+        when(mockRemoteConfigStore.dataCenterCountries).thenReturn(['NL', 'DE']);
+
+        await bannersStore.shownBanners;
 
         expect(bannersStore.banners, BannerType.values);
       });
 
-      test('returns empty list when shown banners and subscription status are null', () {
+      test('returns empty list when shown banners and subscription status are null', () async {
         when(mockApiService.getShownBanners()).thenAnswer((_) async => []);
         when(mockSubscriptionStore.isSubscribed).thenReturn(null);
+
+        await bannersStore.shownBanners;
 
         expect(bannersStore.banners, []);
       });
 
-      test('excludes shown banners from the list', () {
-        when(mockApiService.getShownBanners()).thenAnswer((_) async => [BannerType.subscription]);
+      test('excludes shown banners from the list', () async {
+        when(mockApiService.getShownBanners()).thenAnswer((_) async => [BannerType.datacenter]);
         when(mockSubscriptionStore.isSubscribed).thenReturn(false);
-        when(mockRemoteConfigStore.dataCenterCountries).thenReturn([]);
+        when(mockRemoteConfigStore.dataCenterCountries).thenReturn(['NL', 'DE']);
+
+        await bannersStore.shownBanners;
 
         expect(
           bannersStore.banners,
-          BannerType.values.where((b) => b != BannerType.subscription).toList(),
+          BannerType.values.where((b) => b != BannerType.datacenter).toList(),
         );
       });
 
-      test('excludes subscription banner when subscribed', () {
+      test('excludes subscription banner when subscribed', () async {
         when(mockApiService.getShownBanners()).thenAnswer((_) async => []);
         when(mockSubscriptionStore.isSubscribed).thenReturn(true);
-        when(mockRemoteConfigStore.dataCenterCountries).thenReturn([]);
+        when(mockRemoteConfigStore.dataCenterCountries).thenReturn(['NL', 'DE']);
+
+        await bannersStore.shownBanners;
 
         expect(
           bannersStore.banners,
@@ -70,10 +75,12 @@ void main() {
         );
       });
 
-      test('excludes datacenter banner when dataCenterCountries is empty', () {
+      test('excludes datacenter banner when dataCenterCountries is empty', () async {
         when(mockApiService.getShownBanners()).thenAnswer((_) async => []);
         when(mockSubscriptionStore.isSubscribed).thenReturn(false);
         when(mockRemoteConfigStore.dataCenterCountries).thenReturn([]);
+
+        await bannersStore.shownBanners;
 
         expect(
           bannersStore.banners,
