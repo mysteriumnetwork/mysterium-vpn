2eba680916e01c5aded714f99b663db1deb9d09b
dmacan <david.macan@zeromolecule.com>
2025-02-11 15:49:42 +0100

    feat: introduce `SalesView` and refactor subscription views

--- lib/views/subscription/widgets/subscription_form.dart
+++ lib/views/subscription/widgets/subscription_form.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:mysterium_vpn/common/enums/analytics_event.dart';
 import 'package:mysterium_vpn/common/hooks/ab_test_hook.dart';
+import 'package:mysterium_vpn/common/hooks/handle_subscribe_to_product_hook.dart';
 import 'package:mysterium_vpn/common/utils/utils.dart';
-import 'package:mysterium_vpn/providers/state_providers.dart';
-import 'package:mysterium_vpn/stores/analytics/analytics_store.dart';
-import 'package:mysterium_vpn/stores/subscription_store.dart';
 import 'package:mysterium_vpn/views/subscription/widgets/subscription_form_variant_a.dart';
 import 'package:mysterium_vpn/views/subscription/widgets/subscription_form_variant_b.dart';
 import 'package:mysterium_vpn/views/subscription/widgets/subscription_form_variant_c.dart';
 import 'package:mysterium_vpn/views/subscription/widgets/subscription_form_variant_d.dart';
-import 'package:mysterium_vpn/views/subscription/widgets/subscription_form_variant_e.dart';
 
 class SubscriptionForm extends HookConsumerWidget {
   const SubscriptionForm({super.key});
@@ -19,15 +15,14 @@ class SubscriptionForm extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final variant = useABTest((store) => store.subscriptionFlowVariant);
-    final subscriptionStore = ref.watch(subscriptionStorePOD);
-    final analyticsStore = ref.read(analyticsStorePOD);
+    final handleSubscribe = useHandleSubscribeToProduct();
 
     final shouldApplyHorizontalPadding = variant != 'D';
     final width = getMediaWidth(context);
     final padding = useMemoized(
       () {
-        var padding = EdgeInsets.only(
-          top: variant != 'E' ? 20 : 0,
+        var padding = const EdgeInsets.only(
+          top: 20,
           bottom: 36,
         );
         if (shouldApplyHorizontalPadding) {
@@ -42,15 +37,6 @@ class SubscriptionForm extends HookConsumerWidget {
       },
       [width, shouldApplyHorizontalPadding, variant],
     );
-
-    Future<void> handleSubscribe(String productId) async {
-      await _subscribeToPackage(
-        analyticsStore,
-        subscriptionStore,
-        subscriptionStore.products.map((e) => e.id).toList(),
-        productId,
-      );
-    }
 
     return Padding(
       padding: padding,
@@ -71,10 +57,6 @@ class SubscriptionForm extends HookConsumerWidget {
             subscribeToPackage: handleSubscribe,
             variant: variant,
           ),
-        'E' => SubscriptionFormVariantE(
-            subscribeToPackage: handleSubscribe,
-            variant: variant,
-          ),
         _ => SubscriptionFormVariantA(
             subscribeToPackage: handleSubscribe,
             variant: variant,
@@ -82,19 +64,4 @@ class SubscriptionForm extends HookConsumerWidget {
       },
     );
   }
-}
-
-Future<void> _subscribeToPackage(
-  AnalyticsStore analyticsStore,
-  SubscriptionStore store,
-  List<String> productIds,
-  String selectedProductId,
-) async {
-  final selectedProduct = store.products.firstWhere((element) => element.id == selectedProductId);
-  analyticsStore.logEvent(
-    AnalyticsEvent.subscriptionNew,
-    parameters: {'item_ids': productIds},
-  );
-
-  store.subscribeToPackage(product: selectedProduct.productDetails);
 }
