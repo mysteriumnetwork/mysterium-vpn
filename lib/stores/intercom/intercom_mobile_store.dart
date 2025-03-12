68d6d5d2fd3bdbdbccc0118a0fa966661b87078c
Kristijan <kmitrikjeski@gmail.com>
2025-03-12 17:16:24 +0100

    FIx intercom init

--- lib/stores/intercom/intercom_mobile_store.dart
+++ lib/stores/intercom/intercom_mobile_store.dart
@@ -5,27 +5,13 @@ import 'package:mysterium_vpn/stores/intercom/intercom_store.dart';
 
 part 'intercom_mobile_store.g.dart';
 
-const String intercomAppId = 'sjkeehf4';
-const String intercomAndroidApiKey = 'android_sdk-f9955e908e48bf630f3f2a6dc6609c3f4b5aa2b8';
-const String intercomIosApiKey = 'ios_sdk-13bd499b260981778455ba0235d7ca612b330582';
-
 // ignore: library_private_types_in_public_api
 class IntercomMobileStore = _IntercomMobileStore with _$IntercomMobileStore;
 
 abstract class _IntercomMobileStore extends IntercomStore with Store {
-  _IntercomMobileStore({required Intercom intercom})
-      : _intercomFuture = ObservableFuture(
-          intercom
-              .initialize(
-                intercomAppId,
-                androidApiKey: intercomAndroidApiKey,
-                iosApiKey: intercomIosApiKey,
-              )
-              .then((_) => intercom),
-        );
+  _IntercomMobileStore({required Intercom intercom}) : _intercom = intercom;
 
-  @observable
-  late ObservableFuture<Intercom> _intercomFuture;
+  final Intercom _intercom;
 
   @observable
   bool _isUserLoggedIn = false;
@@ -34,11 +20,10 @@ abstract class _IntercomMobileStore extends IntercomStore with Store {
   @action
   Future<void> registerUser({String? email}) async {
     try {
-      final intercom = await _intercomFuture;
       if (email != null) {
-        await intercom.loginIdentifiedUser(email: email);
+        await _intercom.loginIdentifiedUser(email: email);
       } else {
-        await intercom.loginUnidentifiedUser();
+        await _intercom.loginUnidentifiedUser();
       }
       _isUserLoggedIn = true;
     } catch (e) {
@@ -49,8 +34,7 @@ abstract class _IntercomMobileStore extends IntercomStore with Store {
   @override
   @action
   Future<void> logout() async {
-    final intercom = await _intercomFuture;
-    await intercom.logout();
+    await _intercom.logout();
   }
 
   @override
@@ -59,8 +43,7 @@ abstract class _IntercomMobileStore extends IntercomStore with Store {
     String email,
     String userId,
   ) async {
-    final intercom = await _intercomFuture;
-    await intercom.updateUser(
+    await _intercom.updateUser(
       email: email,
       userId: userId,
     );
@@ -69,19 +52,17 @@ abstract class _IntercomMobileStore extends IntercomStore with Store {
   @override
   @action
   Future<void> displayMessageComposer(String message) async {
-    final intercom = await _intercomFuture;
-    await intercom.displayMessageComposer(message);
+    await _intercom.displayMessageComposer(message);
   }
 
   @override
   @action
   Future<void> displayMessenger() async {
-    final intercom = await _intercomFuture;
     try {
       if (!_isUserLoggedIn) {
         await registerUser();
       }
-      await intercom.displayMessenger();
+      await _intercom.displayMessenger();
     } catch (e) {
       debugPrint(e.toString());
     }
@@ -90,7 +71,6 @@ abstract class _IntercomMobileStore extends IntercomStore with Store {
   @override
   @action
   Future<void> displayHelpCenter() async {
-    final intercom = await _intercomFuture;
-    await intercom.displayHelpCenter();
+    await _intercom.displayHelpCenter();
   }
 }
--- lib/stores/intercom/intercom_mobile_store.g.dart
+++ lib/stores/intercom/intercom_mobile_store.g.dart
@@ -9,26 +9,6 @@ part of 'intercom_mobile_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$IntercomMobileStore on _IntercomMobileStore, Store {
-  late final _$_intercomFutureAtom =
-      Atom(name: '_IntercomMobileStore._intercomFuture', context: context);
-
-  @override
-  ObservableFuture<Intercom> get _intercomFuture {
-    _$_intercomFutureAtom.reportRead();
-    return super._intercomFuture;
-  }
-
-  bool __intercomFutureIsInitialized = false;
-
-  @override
-  set _intercomFuture(ObservableFuture<Intercom> value) {
-    _$_intercomFutureAtom
-        .reportWrite(value, __intercomFutureIsInitialized ? super._intercomFuture : null, () {
-      super._intercomFuture = value;
-      __intercomFutureIsInitialized = true;
-    });
-  }
-
   late final _$_isUserLoggedInAtom =
       Atom(name: '_IntercomMobileStore._isUserLoggedIn', context: context);
 
