5b11cc7e4ba4f9597d0d3709677e2c083fd0dfb3
dmacan <david.macan@zeromolecule.com>
2025-03-14 12:04:51 +0100

    refactor: remove unused dependencies and imports
    - Removes `mobx` related imports and usage in `home_page.dart`.
    - Removes unused `hooks.dart` import in `subscription_watcher_hook.dart`.

--- lib/common/hooks/subscription_watcher_hook.dart
+++ lib/common/hooks/subscription_watcher_hook.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mobx/mobx.dart';
-import 'package:mysterium_vpn/common/hooks/hooks.dart';
 import 'package:mysterium_vpn/providers/state_providers.dart';
 import 'package:mysterium_vpn/services/auth/auth_status.dart';
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:mobx/mobx.dart';
 import 'package:mysterium_vpn/common/enums/enums.dart';
 import 'package:mysterium_vpn/common/hooks/hooks.dart';
 import 'package:mysterium_vpn/common/layout_builders/screen_type_builder.dart';
