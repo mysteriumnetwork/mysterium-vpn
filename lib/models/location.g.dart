1375860007f42b71db645759128be68e955ce7aa
dmacan <david.macan@zeromolecule.com>
2025-03-14 10:14:48 +0100

    feat: enhance VPN location data management
    This commit introduces improvements to the management of VPN location data by adding `VPNLocations` persistence using Hive.
    
    - Adds `VpnLocationsAdapter` to persist `VPNLocations` to local storage with Hive.
    - Updates `VPNLocations` to be serializable.
    - Adds `getLocations`, `setLocations`, and `watchLocations` in `LocalDBService` to interact with VPN locations data.
    - Registers `VpnLocationsAdapter` in the `Environment` class.
    - Adds `locations_data` box for storing `VPNLocations`.
    - Updates `UserData` box to not compact on open.

--- lib/models/location.g.dart
+++ lib/models/location.g.dart
@@ -6,6 +6,22 @@ part of 'location.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
+_$VPNLocationsImpl _$$VPNLocationsImplFromJson(Map<String, dynamic> json) => _$VPNLocationsImpl(
+      locations: (json['locations'] as List<dynamic>?)
+              ?.map((e) => VPNLocation.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          const [],
+      topLocations: (json['topLocations'] as List<dynamic>?)
+              ?.map((e) => VPNLocation.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          const [],
+    );
+
+Map<String, dynamic> _$$VPNLocationsImplToJson(_$VPNLocationsImpl instance) => <String, dynamic>{
+      'locations': instance.locations,
+      'topLocations': instance.topLocations,
+    };
+
 _$VPNLocationImpl _$$VPNLocationImplFromJson(Map<String, dynamic> json) => _$VPNLocationImpl(
       code: json['code'] as String,
       ipType: $enumDecodeNullable(_$IPTypeEnumMap, json['ipType']) ?? IPType.residential,
---
+++ lib/services/data/local/adapters/vpn_locations_adapter.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:hive/hive.dart';
+import 'package:mysterium_vpn/models/location.dart';
+
+class VpnLocationsAdapter extends TypeAdapter<VPNLocations> {
+  VpnLocationsAdapter({required this.typeId});
+
+  @override
+  final int typeId;
+
+  @override
+  VPNLocations read(BinaryReader reader) {
+    final raw = reader.readString();
+    final json = jsonDecode(raw) as Map<String, dynamic>;
+    return VPNLocations.fromJson(json);
+  }
+
+  @override
+  void write(BinaryWriter writer, VPNLocations obj) {
+    writer.writeString(jsonEncode(obj.toJson()));
+  }
+}
