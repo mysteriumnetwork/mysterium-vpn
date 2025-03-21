41ee54c4d8953ce801d970c4e99d58050dc98d71
dmacan <david.macan@zeromolecule.com>
2025-03-21 15:58:46 +0100

    feat: Enhance header and location search
    - Introduces `HomeAppBar` component for consistent header design.
    - Adds `headerColor` to the color palette for styling the header.
    - Updates `LocationsSearch` to support keyword filtering.
    - Refactors `LocationsSearch` into a separate component with search and clear functionality.
    - Integrates `LocationsSearch` into `HomeAppBar` and locations views.
    - Replaces `HomeMobileAppBar` and `HomeDesktopAppBar` with `HomeAppBar`.
    - Removes unnecessary padding and spacing adjustments in views.
    - Update theme colors to utilize `headerColor` for app bar background.

---
+++ lib/components/home_app_bar.dart
@@ -0,0 +1,67 @@
+import 'package:beamer/beamer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mysterium_vpn/common/enums/enums.dart';
+import 'package:mysterium_vpn/common/styles/assets.dart';
+import 'package:mysterium_vpn/common/utils/utils.dart';
+import 'package:mysterium_vpn/components/svg_icon_button.dart';
+import 'package:mysterium_vpn/providers/state_providers.dart';
+import 'package:mysterium_vpn/views/locations/components/locations_search.dart';
+
+class HomeAppBar extends HookConsumerWidget implements PreferredSizeWidget {
+  const HomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final brightness = Theme.of(context).brightness;
+    final analyticsStore = ref.read(analyticsStorePOD);
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 24),
+        child: Column(
+          spacing: 12,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              spacing: 4,
+              children: [
+                Expanded(
+                  child: SvgPicture.asset(
+                    switch (brightness) {
+                      Brightness.dark => Assets.logoWhiteSvg,
+                      Brightness.light => Assets.logoBlackSvg,
+                    },
+                    height: 24,
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+                SvgIconButton(
+                  onPressed: () => handleOnReportPage(
+                    context: context,
+                    intetcomStore: ref.read(intercomStorePOD),
+                    analyticsStore: ref.read(analyticsStorePOD),
+                  ),
+                  asset: Assets.report,
+                ),
+                SvgIconButton(
+                  onPressed: () {
+                    analyticsStore.logEvent(AnalyticsEvent.openSettings);
+                    context.beamToNamed(Routes.settings.path);
+                  },
+                  asset: Assets.settings,
+                ),
+              ],
+            ),
+            const LocationsSearch(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(86);
+}
