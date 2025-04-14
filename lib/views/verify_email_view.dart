a40c93af6efa96bcfd605b7d66ec634e9c5084c5
Kristijan <kmitrikjeski@gmail.com>
2025-04-14 14:12:04 +0200

    Implement countdown timer

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -4,13 +4,16 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mobx/mobx.dart';
 import 'package:mysterium_vpn/common/enums/enums.dart';
+import 'package:mysterium_vpn/common/hooks/hooks.dart';
 import 'package:mysterium_vpn/common/styles/assets.dart';
+import 'package:mysterium_vpn/common/styles/palette.dart';
 import 'package:mysterium_vpn/common/utils/utils.dart';
 import 'package:mysterium_vpn/components/dialogs/adaptive_action_sheet/adaptive_action_sheet.dart';
 import 'package:mysterium_vpn/components/dialogs/no_mail_app_dialog.dart';
 import 'package:mysterium_vpn/components/easy_button.dart';
 import 'package:mysterium_vpn/components/easy_text.dart';
 import 'package:mysterium_vpn/components/loading_barrier.dart';
+import 'package:mysterium_vpn/components/loading_indicator.dart';
 import 'package:mysterium_vpn/components/svg_icon.dart';
 import 'package:mysterium_vpn/generated/locale_keys.g.dart';
 import 'package:mysterium_vpn/providers/state_providers.dart';
@@ -26,56 +29,89 @@ class VerifyEmailView extends HookConsumerWidget {
     final authStore = ref.watch(authStorePOD);
     final analyticsStore = ref.watch(analyticsStorePOD);
     final height = getMediaHeight(context);
-
+    final timer = useCountdownTimer(
+      initialCountdown: 5,
+    );
     return Observer(
-      builder: (context) => Stack(
-        children: [
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              EasyText(
-                LocaleKeys.checkYourEmail.tr(),
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
-              ).padding(bottom: 20),
-              const SvgIcon(
-                asset: Assets.checkEmail,
-              ).padding(bottom: height * .03),
-              EasyText(
-                LocaleKeys.emailSentTo.tr(namedArgs: {'email': '${authStore.email}'}),
-                maxLines: 2,
-                textAlign: TextAlign.center,
-              ).padding(bottom: height * .02),
-              EasyText(
-                LocaleKeys.linkExpires.tr(),
-                maxLines: 2,
-                textAlign: TextAlign.center,
-              ).padding(bottom: height * .02),
-              EasyText(
-                LocaleKeys.consumeLink.tr(),
-                maxLines: 5,
-                textAlign: TextAlign.center,
-              ).padding(bottom: height * .05),
-              Visibility(
-                visible: isMobile(),
-                child: EasyButton(
-                  text: LocaleKeys.openEmailApp.tr(),
-                  onPressed: () {
-                    analyticsStore.logEvent(AnalyticsEvent.openEmailClicked);
-                    openEmailApp(context, analyticsStore);
-                  },
+      builder: (context) {
+        final signInStatus = authStore.signInFeature.status;
+        return Stack(
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                EasyText(
+                  LocaleKeys.checkYourEmail.tr(),
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ).padding(bottom: 20),
+                const SvgIcon(
+                  asset: Assets.checkEmail,
+                ).padding(bottom: height * .03),
+                EasyText(
+                  LocaleKeys.emailSentTo.tr(namedArgs: {'email': '${authStore.email}'}),
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                ).padding(bottom: height * .02),
+                EasyText(
+                  LocaleKeys.linkExpires.tr(),
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                ).padding(bottom: height * .02),
+                EasyText(
+                  LocaleKeys.consumeLink.tr(),
+                  maxLines: 5,
+                  textAlign: TextAlign.center,
+                ).padding(bottom: height * .05),
+                Visibility(
+                  visible: isMobile(),
+                  child: EasyButton(
+                    width: 200,
+                    text: LocaleKeys.openEmailApp.tr(),
+                    onPressed: () {
+                      analyticsStore.logEvent(AnalyticsEvent.openEmailClicked);
+                      openEmailApp(context, analyticsStore);
+                    },
+                  ).padding(
+                    bottom: 20,
+                  ),
                 ),
-              ),
-            ],
-          ).scrollable().padding(
-                top: 20,
-                bottom: 10,
-                horizontal: getMediaWidth(context) > 650 ? 60 : 20,
-              ),
-          if (authStore.authenticateFeature?.status == FutureStatus.pending)
-            LoadingBarrier(color: Theme.of(context).primaryColor),
-        ],
-      ),
+                EasyButton(
+                  width: 200,
+                  color: Palette.purple,
+                  useSystemColor: false,
+                  onPressed: timer.isActive
+                      ? null
+                      : () async {
+                          analyticsStore.logEvent(AnalyticsEvent.resendEmailClicked);
+                          authStore
+                              .signInwithEmail(email: authStore.email!)
+                              .whenComplete(timer.reset);
+                        },
+                  child: signInStatus == FutureStatus.pending
+                      ? const LoadingIndicator(
+                          radius: 20,
+                          strokeWidth: 1.5,
+                          indicatorColor: Palette.white,
+                        )
+                      : EasyText(
+                          LocaleKeys.sendAgain.plural(timer.countdown),
+                          color: Palette.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
+                ),
+              ],
+            ).scrollable().padding(
+                  top: 20,
+                  bottom: 10,
+                  horizontal: getMediaWidth(context) > 650 ? 60 : 20,
+                ),
+            if (authStore.authenticateFeature?.status == FutureStatus.pending)
+              LoadingBarrier(color: Theme.of(context).primaryColor),
+          ],
+        );
+      },
     );
   }
 
