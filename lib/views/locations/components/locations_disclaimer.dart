96241c77aaa56f9a811bfbe937e66f2f4aefa7a3
Kristijan <kmitrikjeski@gmail.com>
2025-04-15 15:08:53 +0200

    Add dismiss option for secondary banners

--- lib/views/locations/components/locations_disclaimer.dart
+++ lib/views/locations/components/locations_disclaimer.dart
@@ -1,39 +1,73 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart' hide Banner;
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mysterium_vpn/common/enums/enums.dart';
 import 'package:mysterium_vpn/components/banners/banner.dart';
 import 'package:mysterium_vpn/components/easy_text.dart';
 import 'package:mysterium_vpn/generated/locale_keys.g.dart';
+import 'package:mysterium_vpn/providers/state_providers.dart';
 
-class LocationsDisclaimer extends StatelessWidget {
+class LocationsDisclaimer extends HookConsumerWidget {
   const LocationsDisclaimer({
     required this.text,
+    required this.bannerType,
     super.key,
   });
 
-  factory LocationsDisclaimer.residential() =>
-      LocationsDisclaimer(text: LocaleKeys.ipTypeResidentialDisclaimer.tr());
+  factory LocationsDisclaimer.residential() => LocationsDisclaimer(
+        text: LocaleKeys.ipTypeResidentialDisclaimer.tr(),
+        bannerType: BannerType.residentialIPs,
+      );
 
-  factory LocationsDisclaimer.dataCenter() =>
-      LocationsDisclaimer(text: LocaleKeys.ipTypeDataCenterDisclaimer.tr());
+  factory LocationsDisclaimer.dataCenter() => LocationsDisclaimer(
+        text: LocaleKeys.ipTypeDataCenterDisclaimer.tr(),
+        bannerType: BannerType.highSpeedIPs,
+      );
 
   final String text;
+  final BannerType bannerType;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
-    return Banner(
-      color: theme.colorScheme.surfaceContainerHigh,
-      borderRadius: BorderRadius.circular(12),
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      title: SizedBox(
-        width: double.infinity,
-        child: EasyText(
-          text,
-          fontSize: 12,
-          maxLines: 2,
-          textAlign: TextAlign.left,
-        ),
-      ),
+    final bannersStore = ref.watch(bannersStorePOD);
+    final analyticsStore = ref.watch(analyticsStorePOD);
+    void handleDismiss() {
+      if (bannerType == BannerType.residentialIPs) {
+        bannersStore.setShown(BannerType.residentialIPs);
+        analyticsStore.logBannerClose(BannerType.residentialIPs);
+      } else if (bannerType == BannerType.highSpeedIPs) {
+        bannersStore.setShown(BannerType.highSpeedIPs);
+        analyticsStore.logBannerClose(BannerType.highSpeedIPs);
+      }
+    }
+
+    return Observer(
+      builder: (context) {
+        if (!bannersStore.canShow(bannerType)) {
+          return const SizedBox.shrink();
+        }
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 30),
+          child: Banner(
+            color: theme.colorScheme.surfaceContainerHigh,
+            borderRadius: BorderRadius.circular(12),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            onDismiss: handleDismiss,
+            title: SizedBox(
+              width: double.infinity,
+              child: EasyText(
+                text,
+                fontSize: 12,
+                maxLines: 2,
+                textAlign: TextAlign.left,
+              ),
+            ),
+            mainBanner: false,
+          ),
+        );
+      },
     );
   }
 }
