1a1e4524a6366b1e827344edd4c3a86c64e6132c
dmacan <david.macan@zeromolecule.com>
2025-04-16 17:03:21 +0200

    feat: Add shadows to the left panel in desktop view
    - Adds shadows to the left panel in desktop view.
    - Uses `useScaffoldBrightness` hook for brightness detection.
    - Adjusts shadow color and blur based on light/dark mode.
    - Wraps `CustomScrollView` with `DecoratedBox` to apply shadow effect.
    - Sets the width of the HomeDesktopLeftPanel.

--- lib/views/home/home_desktop_view.dart
+++ lib/views/home/home_desktop_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mysterium_vpn/common/configurations/breakpoint_configuration.dart';
+import 'package:mysterium_vpn/common/hooks/responsive_value_hook.dart';
 import 'package:mysterium_vpn/views/home/desktop_left_panel.dart';
 import 'package:mysterium_vpn/views/home/desktop_right_panel.dart';
 
@@ -7,15 +9,18 @@ class HomeDesktopView extends HookConsumerWidget {
   const HomeDesktopView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) => const Row(
+  Widget build(BuildContext context, WidgetRef ref) => const Stack(
+        clipBehavior: Clip.none,
+        fit: StackFit.passthrough,
+        alignment: Alignment.topLeft,
         children: [
-          Flexible(
-            flex: 6,
-            child: HomeDesktopLeftPanel(),
-          ),
-          Flexible(
-            flex: 5,
+          Padding(
+            padding: EdgeInsets.only(left: 500),
             child: HomeDesktopRightPanel(),
+          ),
+          SizedBox(
+            width: 500,
+            child: HomeDesktopLeftPanel(),
           ),
         ],
       );
