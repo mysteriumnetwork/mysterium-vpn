daf29f257d67784f386e7c688b3066341d213dcc
dmacan <david.macan@zeromolecule.com>
2025-04-18 10:10:56 +0200

    Refactor: Update map zoom levels and remove rotation
    - Updates the minimum and maximum zoom levels for the world map.
    - Removes the rotation flag from the map interaction options.
    - Uses a constant for map zoom levels instead of hardcoding.

--- lib/views/home/world_map_tiles_layer.dart
+++ lib/views/home/world_map_tiles_layer.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -16,8 +17,8 @@ class WorldMapTilesLayer extends HookWidget {
         Brightness.light => 'assets/map_tiles/light/{z}/{x}/{y}.png',
       },
       tileProvider: AssetTileProvider(),
-      minNativeZoom: 3,
-      maxNativeZoom: 4,
+      minNativeZoom: kMapZoomLevels.min.round(),
+      maxNativeZoom: kMapZoomLevels.max.round(),
       tileBounds: kWorldBounds,
     );
   }
