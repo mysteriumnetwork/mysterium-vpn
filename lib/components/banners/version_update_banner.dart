6f09f84c45be10247603bbb8a2f4e13041a7b503
Kristijan <kmitrikjeski@gmail.com>
2025-04-22 15:33:10 +0200

    Add new app version banner

---
+++ lib/components/banners/version_update_banner.dart
@@ -0,0 +1,47 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart' hide Banner;
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mysterium_vpn/common/enums/enums.dart';
+import 'package:mysterium_vpn/common/styles/assets.dart';
+import 'package:mysterium_vpn/common/utils/utils.dart';
+import 'package:mysterium_vpn/components/banners/banner.dart';
+import 'package:mysterium_vpn/components/banners/banner_body.dart';
+import 'package:mysterium_vpn/components/banners/banner_cta.dart';
+import 'package:mysterium_vpn/components/banners/banner_title.dart';
+import 'package:mysterium_vpn/components/svg_icon.dart';
+import 'package:mysterium_vpn/generated/locale_keys.g.dart';
+import 'package:mysterium_vpn/providers/state_providers.dart';
+
+class AppVersionUpdateBanner extends HookConsumerWidget {
+  const AppVersionUpdateBanner({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final analyticsStore = ref.watch(analyticsStorePOD);
+    final bannersStore = ref.watch(bannersStorePOD);
+
+    Future<void> handlePressed() async {
+      analyticsStore.logBannerClick(BannerType.appUpdateAvailable);
+      await openAppStorePage();
+    }
+
+    void handleDismiss() {
+      analyticsStore.logBannerClose(BannerType.appUpdateAvailable);
+      bannersStore.setShown(BannerType.appUpdateAvailable);
+    }
+
+    return Banner(
+      title: BannerTitle(
+        icon: const SvgIcon(asset: Assets.appUpdate),
+        text: LocaleKeys.appUpdateAvailableTitle.tr(),
+      ),
+      body: BannerBody(text: LocaleKeys.appUpdateAvailableDesc.tr()),
+      cta: BannerCTA(
+        text: LocaleKeys.updateBtn.tr(),
+        onPressed: handlePressed,
+      ),
+      onPressed: handlePressed,
+      onDismiss: handleDismiss,
+    );
+  }
+}
