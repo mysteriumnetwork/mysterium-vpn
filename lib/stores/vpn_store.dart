375d0faa2e31888de20a57dec117ce460f294090
Kristijan <kmitrikjeski@gmail.com>
2025-04-24 18:16:15 +0200

    Handle connect button

--- lib/stores/vpn_store.dart
+++ lib/stores/vpn_store.dart
@@ -457,9 +457,16 @@ abstract class _VpnStore with Store {
     if (_subscriptionStore.subscriptionFuture.status == FutureStatus.pending) {
       return;
     }
-    final subscription = await _subscriptionStore.subscriptionFuture;
-    if (!subscription.active) {
-      throw const SubscriptionRequiredException();
+    try {
+      final subscription = await _subscriptionStore.subscriptionFuture;
+      if (!subscription.active) {
+        throw const SubscriptionRequiredException();
+      }
+    } catch (e) {
+      if (e is! SubscriptionRequiredException) {
+        _subscriptionStore.refreshSubscription();
+      }
+      rethrow;
     }
   }
 
