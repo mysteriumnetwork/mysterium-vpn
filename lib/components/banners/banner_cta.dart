fdf8c953f85da989113d56286271d82620e3d563
dmacan <david.macan@zeromolecule.com>
2025-04-24 13:12:34 +0200

    feat: implement dynamic banner styles and add pale yellow color to palette

--- lib/components/banners/banner_cta.dart
+++ lib/components/banners/banner_cta.dart
@@ -1,29 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:mysterium_vpn/common/styles/palette.dart';
+import 'package:mysterium_vpn/components/banners/banner.dart';
 
 class BannerCTA extends StatelessWidget {
   const BannerCTA({
     required this.text,
     required this.onPressed,
-    this.color = Palette.purple,
     super.key,
   });
 
   final String text;
   final VoidCallback onPressed;
-  final Color color;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final bannerStyle = BannerStyle.of(context);
+
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
-        backgroundColor: color,
+        backgroundColor: bannerStyle.ctaBackgroundColor,
         elevation: 0,
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         minimumSize: const Size(130, 32),
-        foregroundColor: Palette.white,
+        foregroundColor: bannerStyle.ctaForegroundColor,
         visualDensity: VisualDensity.comfortable,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
       ),
@@ -31,7 +31,7 @@ class BannerCTA extends StatelessWidget {
         text,
         style: theme.textTheme.labelMedium!.copyWith(
           fontWeight: FontWeight.w700,
-          color: Palette.white,
+          color: bannerStyle.ctaForegroundColor,
           fontSize: 12,
         ),
       ),
