cb40b51016ec590f4ad84de42ea88d00d6434fb9
Kristijan <kmitrikjeski@gmail.com>
2025-05-07 16:11:14 +0200

    Log api exceptions to analytics

--- lib/common/exceptions/api.dart
+++ lib/common/exceptions/api.dart
@@ -1,10 +1,16 @@
 import 'package:dio/dio.dart';
 
 class ApiException extends DioException {
-  ApiException(RequestOptions requestOptions, String message, this.code, this.identifier)
-      : super(requestOptions: requestOptions, message: message);
+  ApiException(
+    RequestOptions requestOptions,
+    String message, {
+    required this.code,
+    required this.identifier,
+    required this.endpoint,
+  }) : super(requestOptions: requestOptions, message: message);
   @override
   String get message => super.message!;
   String identifier;
   final int code;
+  final String endpoint;
 }
