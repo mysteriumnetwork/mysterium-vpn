6e5560378decc225b475e86f050db038e5caf4cb
Kristijan <kmitrikjeski@gmail.com>
2025-05-22 09:14:02 +0200

    Generate Rate connection API endpoint

--- packages/vpn_api/lib/src/api/connection.dart
+++ packages/vpn_api/lib/src/api/connection.dart
@@ -14,6 +14,7 @@ import 'package:vpn_api/src/model/connection_config_response.dart';
 import 'package:vpn_api/src/model/connection_location.dart';
 import 'package:vpn_api/src/model/https_connect_request.dart';
 import 'package:vpn_api/src/model/proxy_connect_response.dart';
+import 'package:vpn_api/src/model/rate_connection_request.dart';
 import 'package:vpn_api/src/model/wireguard_connect_request.dart';
 import 'package:vpn_api/src/model/wireguard_connect_response.dart';
 
@@ -445,5 +446,70 @@ class Connection {
       statusMessage: _response.statusMessage,
       extra: _response.extra,
     );
+  }
+
+  /// Rate connection
+  ///
+  ///
+  /// Parameters:
+  /// * [rateConnectionRequest]
+  /// * [cancelToken] - A [CancelToken] that can be used to cancel the operation
+  /// * [headers] - Can be used to add additional headers to the request
+  /// * [extras] - Can be used to add flags to the request
+  /// * [validateStatus] - A [ValidateStatus] callback that can be used to determine request success based on the HTTP status of the response
+  /// * [onSendProgress] - A [ProgressCallback] that can be used to get the send progress
+  /// * [onReceiveProgress] - A [ProgressCallback] that can be used to get the receive progress
+  ///
+  /// Returns a [Future]
+  /// Throws [DioException] if API call or serialization fails
+  Future<Response<void>> rateConnection({
+    RateConnectionRequest? rateConnectionRequest,
+    CancelToken? cancelToken,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? extra,
+    ValidateStatus? validateStatus,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    final _path = r'/connection/rate-connection';
+    final _options = Options(
+      method: r'POST',
+      headers: <String, dynamic>{
+        ...?headers,
+      },
+      extra: <String, dynamic>{
+        'secure': <Map<String, String>>[],
+        ...?extra,
+      },
+      contentType: 'application/json',
+      validateStatus: validateStatus,
+    );
+
+    dynamic _bodyData;
+
+    try {
+      _bodyData = jsonEncode(rateConnectionRequest);
+    } catch (error, stackTrace) {
+      throw DioException(
+        requestOptions: _options.compose(
+          _dio.options,
+          _path,
+        ),
+        type: DioExceptionType.unknown,
+        error: error,
+        stackTrace: stackTrace,
+      );
+    }
+
+    final _response = await _dio.request<Object>(
+      _path,
+      data: _bodyData,
+      options: _options,
+      cancelToken: cancelToken,
+      onSendProgress: onSendProgress,
+      onReceiveProgress: onReceiveProgress,
+    );
+
+    return _response;
   }
 }
