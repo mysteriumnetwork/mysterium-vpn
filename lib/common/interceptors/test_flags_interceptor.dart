4e06f0e2da2327cffde4cffce105e22ca7e9f2a3
dmacan <david.macan@zeromolecule.com>
2025-06-13 17:17:33 +0200

    feat: Add quick_auth query parameter for magic link requests

--- lib/common/interceptors/test_flags_interceptor.dart
+++ lib/common/interceptors/test_flags_interceptor.dart
@@ -5,4 +5,20 @@ class TestFlagsInterceptor extends InterceptorsWrapper {
   TestFlagsInterceptor(this.environment);
 
   final FlavorConfig environment;
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    if (options.path.endsWith('/magic-link')) {
+      // if the request is for magic link, add quick_auth query parameter to request
+      return handler.next(
+        options
+          ..queryParameters = {
+            ...options.queryParameters,
+            'quick_auth': environment.values.quickAuth,
+          },
+      );
+    }
+
+    super.onRequest(options, handler);
+  }
 }
