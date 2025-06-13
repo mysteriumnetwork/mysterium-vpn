021193a3a02e6d1081120dca637e0f88d63aa10b
dmacan <david.macan@zeromolecule.com>
2025-06-13 16:29:10 +0200

    Update base URL for dev flavor
    The `baseUrl` in `FlavorValues` for the `dev` flavor has been changed to `https://bdd15f1f4419-2107680835833771469.ngrok-free.app/api/v1`.

--- lib/models/flavor_config.dart
+++ lib/models/flavor_config.dart
@@ -59,7 +59,7 @@ class FlavorValues {
     required bool quickAuth,
   }) =>
       FlavorValues._(
-        baseUrl: 'https://api-test.mysteriumvpn.com/api/v1',
+        baseUrl: 'https://bdd15f1f4419-2107680835833771469.ngrok-free.app/api/v1',
         mqttUrl: 'wss://events-test.mysteriumvpn.com/ws',
         mqttUsername: 'dvpn',
         mqttPassword: 'VWm2UJVV7BYr6p3S8ZZZ',
@@ -110,11 +110,13 @@ class FlavorConfig {
     required BuildInfo buildInfo,
   }) =>
       FlavorConfig._internal(flavor, values, buildInfo);
+
   FlavorConfig._internal(
     this.flavor,
     this.values,
     this.buildInfo,
   );
+
   final Flavor flavor;
   final FlavorValues values;
   final BuildInfo buildInfo;
