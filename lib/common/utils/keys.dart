75cf7b341b167fd0571764246accda8d2ce15ddc
dmacan <david.macan@zeromolecule.com>
2025-06-18 10:31:28 +0200

    test: Enhance integration tests with new keys for UI elements and improve navigation flow

--- lib/common/utils/keys.dart
+++ lib/common/utils/keys.dart
@@ -4,6 +4,18 @@ typedef K = Keys;
 
 class Keys {
   static const backButton = Key('backButton');
+
+  // login
+  static const loginPage = Key('loginPage');
   static const loginEmailField = Key('loginEmailField');
   static const loginButton = Key('loginButton');
+
+  // home
+  static const homePage = Key('homePage');
+  static const unauthenticatedBanner = Key('unauthenticatedBanner');
+  static const subscriptionBanner = Key('subscriptionBanner');
+  static const subscriptionBannerCTA = Key('subscriptionBannerCTA');
+
+  // subscription
+  static const subscriptionPage = Key('subscriptionPage');
 }
