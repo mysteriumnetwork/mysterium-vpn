3f684702dc3038f48e476b82b058a4f00772b26a
Kristijan <kmitrikjeski@gmail.com>
2025-07-11 14:50:24 +0200

    Make SecureStorageService.init async and handle RESET_FAILED
    Changed SecureStorageService.init to be asynchronous and await readAll to ensure storage is initialized properly. Added handling for PlatformException with code 'RESET_FAILED' to reset corrupted storage, improving robustness during encryption algorithm changes.

--- lib/entrypoints/environment.dart
+++ lib/entrypoints/environment.dart
@@ -96,7 +96,7 @@ class Environment {
     flavorConfig = await _setupFlavor();
     await _setupTrayIcon(flavorConfig);
     await SharedPreferenceService.instance.init();
-    SecureStorageService.instance.init(flavorConfig);
+    await SecureStorageService.instance.init(flavorConfig);
     await EasyLocalization.ensureInitialized();
     await LocalDBService.initialize();
     providerContainer = ProviderContainer(
