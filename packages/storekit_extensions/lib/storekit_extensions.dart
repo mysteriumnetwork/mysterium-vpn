1c395df062d9b8b6f1e369e04ebdcc21437f2b55
Kristijan <kmitrikjeski@gmail.com>
2025-07-29 12:29:12 +0200

    Remove storekit_extensions plugin package
    Deleted all files related to the storekit_extensions Flutter plugin, including source code, platform implementations, configuration, and documentation. This removes the plugin from the repository.

--- packages/storekit_extensions/lib/storekit_extensions.dart
+++
@@ -1,9 +0,0 @@
-import 'storekit_extensions_platform_interface.dart';
-
-class StorekitExtensions {
-  const StorekitExtensions();
-
-  Future<bool> isEligibleForIntroOffer(String productId) {
-    return StorekitExtensionsPlatform.instance.isEligibleForIntroOffer(productId);
-  }
-}
--- packages/storekit_extensions/lib/storekit_extensions_windows.dart
+++
@@ -1,3 +0,0 @@
-import 'package:storekit_extensions/storekit_extensions_platform_interface.dart';
-
-class StoreKitExtensionsWindows extends StorekitExtensionsPlatform {}
