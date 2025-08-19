e295544fc0e43dcdf516f34d147984a83a92d292
dmacan <david.macan@zeromolecule.com>
2025-08-19 10:33:37 +0200

    fix: Enhance coordinate retrieval logic to support country-based fallback

--- lib/common/hooks/current_ip_coordinates_hook.dart
+++ lib/common/hooks/current_ip_coordinates_hook.dart
@@ -1,4 +1,5 @@
 import 'package:latlong2/latlong.dart';
+import 'package:mysterium_vpn/common/extensions/vpn_location.dart';
 import 'package:mysterium_vpn/common/hooks/hooks.dart';
 import 'package:mysterium_vpn/providers/state_providers.dart';
 import 'package:wireguard_dart/connection_status.dart';
@@ -14,7 +15,13 @@ LatLng? useCurrentIPCoordinates() {
           vpnStore.connectionStatus == ConnectionStatus.connected) {
         final location = vpnStore.location ?? vpnStore.connectingLocation;
         if (location != null) {
-          return latLngStore.coordinatesFor(location);
+          var coordinates = latLngStore.coordinatesFor(location);
+          if (coordinates == null && !location.isCountry) {
+            coordinates = latLngStore.coordinatesForCountry(location.countryCode);
+          }
+          if (coordinates != null) {
+            return coordinates;
+          }
         }
       }
 
