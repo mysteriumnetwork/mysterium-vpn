232131f95310dae58811bd190734df07ff982680
Kristijan <kmitrikjeski@gmail.com>
2025-09-23 17:49:03 +0200

    Add protocol picker availability check to VpnProtocolStore
    VpnProtocolStore now depends on RemoteConfigStore to determine if the protocol picker is available. If unavailable, protocol is forced to OpenVPN and setProtocol is a no-op. Updated provider and tests to support and verify this behavior.

--- test/stores/vpn_protocol_store_test.dart
+++ test/stores/vpn_protocol_store_test.dart
@@ -4,6 +4,7 @@ import 'package:mockito/mockito.dart';
 import 'package:mysterium_vpn/common/enums/enums.dart';
 import 'package:mysterium_vpn/services/data/local/local_db_service.dart';
 import 'package:mysterium_vpn/stores/analytics/analytics_store.dart';
+import 'package:mysterium_vpn/stores/remote_config/remote_config_store.dart';
 import 'package:mysterium_vpn/stores/vpn_protocol_store.dart';
 
 import 'vpn_protocol_store_test.mocks.dart';
@@ -11,20 +12,24 @@ import 'vpn_protocol_store_test.mocks.dart';
 @GenerateNiceMocks([
   MockSpec<LocalDBService>(),
   MockSpec<AnalyticsStore>(),
+  MockSpec<RemoteConfigStore>(),
 ])
 void main() {
   late MockLocalDBService mockLocalDB;
   late MockAnalyticsStore mockAnalyticsStore;
+  late MockRemoteConfigStore mockRemoteConfigStore;
   late VpnProtocolStore store;
 
   setUp(() {
     mockLocalDB = MockLocalDBService();
     mockAnalyticsStore = MockAnalyticsStore();
-    store = VpnProtocolStore(mockLocalDB, mockAnalyticsStore);
+    mockRemoteConfigStore = MockRemoteConfigStore();
+    store = VpnProtocolStore(mockLocalDB, mockAnalyticsStore, mockRemoteConfigStore);
   });
 
   group('getProtocol', () {
     test('loads protocol from local DB and sets observable', () async {
+      when(mockRemoteConfigStore.isProtocolPickerAvailable).thenReturn(true);
       when(mockLocalDB.getProtocolType()).thenAnswer((_) async => ProtocolType.openvpn);
 
       final protocol = await store.getProtocol();
@@ -33,7 +38,18 @@ void main() {
       expect(store.protocol, ProtocolType.openvpn);
     });
 
+    test('returns openvpn if protocol picker is not available', () async {
+      when(mockRemoteConfigStore.isProtocolPickerAvailable).thenReturn(false);
+
+      final protocol = await store.getProtocol();
+
+      expect(protocol, ProtocolType.openvpn);
+      expect(store.protocol, ProtocolType.openvpn);
+      verifyNever(mockLocalDB.getProtocolType());
+    });
+
     test('throws on error', () async {
+      when(mockRemoteConfigStore.isProtocolPickerAvailable).thenReturn(true);
       when(mockLocalDB.getProtocolType()).thenThrow(Exception('fail'));
 
       expect(() => store.getProtocol(), throwsException);
@@ -42,6 +58,7 @@ void main() {
 
   group('setProtocol', () {
     test('sets protocol, saves to local DB, and logs analytics', () async {
+      when(mockRemoteConfigStore.isProtocolPickerAvailable).thenReturn(true);
       when(mockLocalDB.setProtocolType(ProtocolType.openvpn)).thenAnswer((_) async => {});
 
       await store.setProtocol(ProtocolType.openvpn);
@@ -56,7 +73,16 @@ void main() {
       ).called(1);
     });
 
+    test('does nothing if protocol picker is not available', () async {
+      when(mockRemoteConfigStore.isProtocolPickerAvailable).thenReturn(false);
+
+      await store.setProtocol(ProtocolType.openvpn);
+      verifyNever(mockLocalDB.setProtocolType(any));
+      verifyNever(mockAnalyticsStore.logEvent(any, parameters: anyNamed('parameters')));
+    });
+
     test('logs analytics error on failure', () async {
+      when(mockRemoteConfigStore.isProtocolPickerAvailable).thenReturn(true);
       when(mockLocalDB.setProtocolType(ProtocolType.openvpn)).thenThrow(Exception('fail'));
 
       expect(() => store.setProtocol(ProtocolType.openvpn), throwsException);
