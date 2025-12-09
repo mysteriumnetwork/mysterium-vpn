ddeabdcc4604890cbf5b3d82cfd40615ae766df0
Kristijan <kmitrikjeski@gmail.com>
2025-12-09 12:14:42 +0100

    Remove unused import of auth_status.dart
    Eliminated unnecessary imports of 'auth_status.dart' from subscription_watcher_hook.dart and vpn_guard.dart to clean up the codebase.

--- lib/common/hooks/subscription_watcher_hook.dart
+++ lib/common/hooks/subscription_watcher_hook.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mobx/mobx.dart';
-import 'package:mysterium_vpn/common/enums/auth_status.dart';
 import 'package:mysterium_vpn/providers/state_providers.dart';
 
 void useSubscriptionWatcher() {
