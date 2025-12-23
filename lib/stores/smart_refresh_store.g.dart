0abd5459b2cd15b0509cb8fa46d46fe7f1c02dfe
dmacan <david.macan@zeromolecule.com>
2025-12-23 17:09:02 +0100

    fix: implement smart refresh store to enhance subscription location updates

---
+++ lib/stores/smart_refresh_store.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'smart_refresh_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$SmartRefreshStore on _SmartRefreshStore, Store {
+  @override
+  String toString() {
+    return '''
+
+    ''';
+  }
+}
