83b03cbc4b9a8e5cda5ea959f5f11b40cd09f828
Kristijan <kmitrikjeski@gmail.com>
2026-01-08 09:47:33 +0100

    Clear Vulkan cache on MainActivity startup
    Added logic to remove Vulkan-related cache files and directories during MainActivity's onCreate. This helps prevent issues related to stale or corrupted Vulkan shader caches.

--- android/app/src/main/kotlin/com/mysteriumvpn/android/MainActivity.kt
+++ android/app/src/main/kotlin/com/mysteriumvpn/android/MainActivity.kt
@@ -1,6 +1,37 @@
 package com.mysteriumvpn.android
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import java.io.File
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        clearVulkanCache()
+        super.onCreate(savedInstanceState)
+    }
+    
+    private fun clearVulkanCache() {
+        try {
+            val cacheDirs = listOf(
+                File(applicationContext.cacheDir, "vulkan_shader_cache"),
+                File(applicationContext.cacheDir, "code_cache/vulkan"),
+                File(applicationContext.cacheDir, "VkPipelineCache"),
+                File(applicationContext.cacheDir, "gpu_cache")
+            )
+            
+            cacheDirs.forEach { cacheDir ->
+                if (cacheDir.exists()) {
+                    cacheDir.deleteRecursively()
+                }
+            }
+            
+            applicationContext.cacheDir.listFiles()?.forEach { file ->
+                if (file.isFile && file.name.endsWith(".vkcache")) {
+                    file.delete()
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
