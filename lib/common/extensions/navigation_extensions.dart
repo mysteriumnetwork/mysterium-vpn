6f193af86d0387da031e39a9311f81bf1bba0d69
Kristijan <kmitrikjeski@gmail.com>
2026-01-16 17:49:04 +0100

    Update navigation_extensions.dart

--- lib/common/extensions/navigation_extensions.dart
+++ lib/common/extensions/navigation_extensions.dart
@@ -6,6 +6,17 @@ import 'package:mysterium_vpn/pages/subscription_plans_modal_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 extension NavigationExtensions on BeamerDelegate {
+  /// Navigates based on the given [url], handling internal routes and external links.
+  ///
+  /// The [url] is interpreted according to the following rules, in order:
+  ///  * `'/subscribe'` &mdash; opens the subscription plans modal using [context].
+  ///  * A value matching [Routes.values] or [Routes.path] &mdash; performs internal
+  ///    navigation via [beamToNamed] to the corresponding route path.
+  ///  * Any other value that can be parsed as a [Uri] and launched by
+  ///    `url_launcher` &mdash; is opened as an external URL.
+  ///
+  /// If [url] cannot be parsed as a [Uri], or if `canLaunchUrl` returns `false`,
+  /// the method returns without performing any navigation.
   Future<void> navigateToUrl(String url, BuildContext context) async {
     if (url == '/subscribe') {
       showSubscriptionPlansModalPage(context);
