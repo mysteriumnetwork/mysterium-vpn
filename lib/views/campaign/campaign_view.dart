9cacf72c0afb87740889cfb9010600b301d31db0
Valdas Petrulis <petrulis.valdas@gmail.com>
2026-03-25 14:18:29 +0200

    Call application from Landing screen
     Conflicts:
    	lib/views/campaign/campaign_view.dart
    
     Conflicts:
    	lib/views/campaign/campaign_view.dart

--- lib/views/campaign/campaign_view.dart
+++ lib/views/campaign/campaign_view.dart
@@ -1,19 +1,26 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mysterium_vpn/common/hooks/hooks.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 // Import for Android features.
 //import 'package:webview_flutter_android/webview_flutter_android.dart';
 // Import for iOS/macOS features.
 //import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
-class CampaignWebViewScreen extends StatefulWidget {
+class CampaignWebViewScreen extends StatefulHookConsumerWidget {
   const CampaignWebViewScreen({super.key});
 
   @override
-  State<CampaignWebViewScreen> createState() => _WebViewScreenState();
+  ConsumerState<CampaignWebViewScreen> createState() => _WebViewScreenState();
 }
 
-class _WebViewScreenState extends State<CampaignWebViewScreen> {
+class _WebViewScreenState extends ConsumerState<CampaignWebViewScreen> {
   late WebViewController _controller;
+  late FutureOr<void> Function({bool manageSubscription}) _handleSubscribe;
+
   bool _isLoading = true;
 
   @override
@@ -35,7 +42,8 @@ class _WebViewScreenState extends State<CampaignWebViewScreen> {
         ),
       )
       ..addJavaScriptChannel('CampaignBridge', onMessageReceived: _onJsMessage)
-      ..loadRequest(Uri.parse('https://www.mysteriumvpn.com/features'));
+      //..loadRequest(Uri.parse('https://www.mysteriumvpn.com/features'))
+      ..loadRequest(Uri.parse('http://localhost:3000/campaign'));
     super.initState();
   }
 
@@ -45,21 +53,47 @@ class _WebViewScreenState extends State<CampaignWebViewScreen> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    body: Stack(
-      children: [
-        WebViewWidget(controller: _controller),
-        if (_isLoading) const Center(child: CircularProgressIndicator()),
-      ],
-    ),
-  );
+  Widget build(BuildContext context) {
+    _handleSubscribe = useHandleSubscribe();
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          WebViewWidget(controller: _controller),
+          if (_isLoading) const Center(child: CircularProgressIndicator()),
+        ],
+      ),
+    );
+  }
 
   bool _isAllowedDomain(Uri uri) {
     final host = uri.host;
-    return host.endsWith('mysterium.network') || host.endsWith('mysteriumvpn.com');
+    return host.endsWith('mysterium.network') ||
+        host.endsWith('mysteriumvpn.com') ||
+        host.endsWith('localhost');
   }
 
   void _onJsMessage(JavaScriptMessage message) {
+    final data = jsonDecode(message.message) as Map<String, dynamic>;
+    final type = data['type'] as String?;
 
+    switch (type) {
+      case 'DEBUG_PING':
+        break;
+      case 'SUBSCRIBE':
+        _handleSubscribe();
+        break;
+      case 'DISCOUNT_VALIDATE':
+        //_handleDiscountValidate(data['payload'] as Map<String, dynamic>?);
+        break;
+      case 'OPEN_INTERCOM':
+        //_handleOpenIntercom(data['payload'] as Map<String, dynamic>?);
+        break;
+      case 'CLOSE_LS':
+        //_handleClose();
+        break;
+      default:
+        break;
+    }
   }
 }
