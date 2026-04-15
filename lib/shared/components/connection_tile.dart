96fa7845eeebff8f7ccc5334280ef1cbe84bfe08
Kristijan Mitrikjeski <kmitrikjeski@gmail.com>
2026-04-15 12:46:37 +0200

    Move Observer into _ConnectionTileContent
    Move the MobX Observer out of ConnectionTile and into the _ConnectionTileContent.build method. Remove the BuildContext parameter from _ConnectionTileContent and make it a const widget, localizing reactive updates to the inner widget and simplifying the constructor. No intended behavior changes; mainly a refactor to reduce context coupling and scope rebuilds.

--- lib/shared/components/connection_tile.dart
+++ lib/shared/components/connection_tile.dart
@@ -38,218 +38,215 @@ class ConnectionTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Column(
-    children: [
-      Observer(builder: (context) => _ConnectionTileContent(context: context)),
-      if (Env.flavor.isDev) const _DevProtocolLabel(),
-    ],
+    children: [const _ConnectionTileContent(), if (Env.flavor.isDev) const _DevProtocolLabel()],
   );
 }
 
 class _ConnectionTileContent extends StatelessWidget {
-  const _ConnectionTileContent({required this.context});
-
-  final BuildContext context;
+  const _ConnectionTileContent();
 
   @override
-  Widget build(BuildContext _) {
-    final connectionDisplayStore = getIt<ConnectionDisplayStore>();
-    final vpnStore = getIt<VpnStore>();
-    final analyticsStore = getIt<AnalyticsStore>();
-    final selectedLocationStore = getIt<SelectedLocationStore>();
-    final locationsStore = getIt<LocationsStore>();
-    final unavailableLocationsStore = getIt<UnavailableLocationsStore>();
-    final subscriptionFeaturesStore = getIt<SubscriptionFeaturesStore>();
-    final subscriptionStore = getIt<SubscriptionStore>();
+  Widget build(BuildContext context) => Observer(
+    builder: (context) {
+      final connectionDisplayStore = getIt<ConnectionDisplayStore>();
+      final vpnStore = getIt<VpnStore>();
+      final analyticsStore = getIt<AnalyticsStore>();
+      final selectedLocationStore = getIt<SelectedLocationStore>();
+      final locationsStore = getIt<LocationsStore>();
+      final unavailableLocationsStore = getIt<UnavailableLocationsStore>();
+      final subscriptionFeaturesStore = getIt<SubscriptionFeaturesStore>();
+      final subscriptionStore = getIt<SubscriptionStore>();
 
-    final hasDifferentSelection = connectionDisplayStore.hasDifferentSelection;
-    final selectedLocation = selectedLocationStore.value;
-    final connectedLocation = vpnStore.location;
-    final isConnected = vpnStore.isConnected;
-    final isLoading = connectionDisplayStore.isLoading;
-    final ipInfo = connectionDisplayStore.connectionIP;
-    final targetLocation = connectionDisplayStore.targetLocation;
-    final intent = connectionDisplayStore.connectionIntent;
-    final displayLocation = connectionDisplayStore.displayLocation;
-    final parentLocation = connectionDisplayStore.parentLocation;
-    final isLocationAvailable = connectionDisplayStore.isLocationAvailable;
-    final connectionRated = connectionDisplayStore.connectionRated;
-    final vpnStatus = vpnStore.vpnStatus;
-    final subscription = subscriptionStore.subscriptionFuture.value;
+      final hasDifferentSelection = connectionDisplayStore.hasDifferentSelection;
+      final selectedLocation = selectedLocationStore.value;
+      final connectedLocation = vpnStore.location;
+      final isConnected = vpnStore.isConnected;
+      final isLoading = connectionDisplayStore.isLoading;
+      final ipInfo = connectionDisplayStore.connectionIP;
+      final targetLocation = connectionDisplayStore.targetLocation;
+      final intent = connectionDisplayStore.connectionIntent;
+      final displayLocation = connectionDisplayStore.displayLocation;
+      final parentLocation = connectionDisplayStore.parentLocation;
+      final isLocationAvailable = connectionDisplayStore.isLocationAvailable;
+      final connectionRated = connectionDisplayStore.connectionRated;
+      final vpnStatus = vpnStore.vpnStatus;
+      final subscription = subscriptionStore.subscriptionFuture.value;
 
-    final needsUpgrade = () {
-      final intentLocation = hasDifferentSelection ? selectedLocation : displayLocation;
-      return intentLocation != null &&
-          LocationMode.from(
-                location: intentLocation,
-                residentialIPsAllowed: subscriptionFeaturesStore.residentialIPsAllowed,
-                unavailableLocations: unavailableLocationsStore.unavailableLocations,
-                subscription: subscription,
-                isConnected: isConnected,
-                isLoading: isLoading,
-                vpnLocation: connectedLocation,
-                connectingLocation: null,
-              ) ==
-              LocationMode.unsupportedByPlan;
-    }();
+      final needsUpgrade = () {
+        final intentLocation = hasDifferentSelection ? selectedLocation : displayLocation;
+        return intentLocation != null &&
+            LocationMode.from(
+                  location: intentLocation,
+                  residentialIPsAllowed: subscriptionFeaturesStore.residentialIPsAllowed,
+                  unavailableLocations: unavailableLocationsStore.unavailableLocations,
+                  subscription: subscription,
+                  isConnected: isConnected,
+                  isLoading: isLoading,
+                  vpnLocation: connectedLocation,
+                  connectingLocation: null,
+                ) ==
+                LocationMode.unsupportedByPlan;
+      }();
 
-    final isMobile = ScreenType.of(context) <= ScreenType.mobile;
+      final isMobile = ScreenType.of(context) <= ScreenType.mobile;
 
-    final MainIpCardStatus status;
-    var noConnectionTitle = LocaleKeys.connectBestServer.tr();
-    var noConnectionDescription = LocaleKeys.orSelectCountryManually.tr();
-    final upgradeLabel = needsUpgrade ? LocaleKeys.subscriptionUpgrade.tr() : null;
-    var connectLabel = upgradeLabel ?? LocaleKeys.connect.tr();
-    var disconnectLabel = upgradeLabel ?? LocaleKeys.disconnect.tr();
+      final MainIpCardStatus status;
+      var noConnectionTitle = LocaleKeys.connectBestServer.tr();
+      var noConnectionDescription = LocaleKeys.orSelectCountryManually.tr();
+      final upgradeLabel = needsUpgrade ? LocaleKeys.subscriptionUpgrade.tr() : null;
+      var connectLabel = upgradeLabel ?? LocaleKeys.connect.tr();
+      var disconnectLabel = upgradeLabel ?? LocaleKeys.disconnect.tr();
 
-    if (hasDifferentSelection && connectedLocation != null) {
-      final connected = connectedLocation;
-      final connectedParent = locationsStore.findParent(connected);
-      final connectedCountry = connectedParent?.getName(context) ?? connected.getName(context);
-      final connectedCity = connectedParent != null ? connected.getName(context) : '';
-      final connectedServiceQuality = connected.ipType == IPType.residential
-          ? LocaleKeys.residential.tr()
-          : LocaleKeys.highSpeed.tr();
-      final connectedIpPoolCount = connected.nodeCount ?? 0;
+      if (hasDifferentSelection && connectedLocation != null) {
+        final connected = connectedLocation;
+        final connectedParent = locationsStore.findParent(connected);
+        final connectedCountry = connectedParent?.getName(context) ?? connected.getName(context);
+        final connectedCity = connectedParent != null ? connected.getName(context) : '';
+        final connectedServiceQuality = connected.ipType == IPType.residential
+            ? LocaleKeys.residential.tr()
+            : LocaleKeys.highSpeed.tr();
+        final connectedIpPoolCount = connected.nodeCount ?? 0;
 
-      final isSelectedUnavailable = unavailableLocationsStore.unavailableLocations.contains(
-        selectedLocation,
-      );
-      final switchLabel = isSelectedUnavailable
-          ? LocaleKeys.locationUnavailableAction.tr()
-          : LocaleKeys.switchToLocationBtn.tr(
-              namedArgs: {'switchLocation': selectedLocation!.getName(context)},
-            );
-
-      if (isMobile) {
-        status = MainIpCardNewIpPreview(
-          country: connectedCountry,
-          countryIcon: CircleFlag(connected.countryCode, size: 32),
-          city: connectedCity,
-          ipAddress: ipInfo ?? '',
-          serviceQuality: connectedServiceQuality,
-          ipPoolCount: connectedIpPoolCount,
-          previewCountry: selectedLocation!.getName(context),
-          previewCountryIcon: CircleFlag(selectedLocation.countryCode, size: 32),
-          switchLabel: switchLabel,
+        final isSelectedUnavailable = unavailableLocationsStore.unavailableLocations.contains(
+          selectedLocation,
         );
+        final switchLabel = isSelectedUnavailable
+            ? LocaleKeys.locationUnavailableAction.tr()
+            : LocaleKeys.switchToLocationBtn.tr(
+                namedArgs: {'switchLocation': selectedLocation!.getName(context)},
+              );
+
+        if (isMobile) {
+          status = MainIpCardNewIpPreview(
+            country: connectedCountry,
+            countryIcon: CircleFlag(connected.countryCode, size: 32),
+            city: connectedCity,
+            ipAddress: ipInfo ?? '',
+            serviceQuality: connectedServiceQuality,
+            ipPoolCount: connectedIpPoolCount,
+            previewCountry: selectedLocation!.getName(context),
+            previewCountryIcon: CircleFlag(selectedLocation.countryCode, size: 32),
+            switchLabel: switchLabel,
+          );
+        } else {
+          final selectedParent = locationsStore.findParent(selectedLocation!);
+          final selectedCountry =
+              selectedParent?.getName(context) ?? selectedLocation.getName(context);
+          final selectedCity = selectedParent != null ? selectedLocation.getName(context) : '';
+
+          status = MainIpCardConnected(
+            country: selectedCountry,
+            countryIcon: CircleFlag(selectedLocation.countryCode, size: 32),
+            city: selectedCity,
+            ipAddress: ipInfo ?? '',
+            serviceQuality: connectedServiceQuality,
+            ipPoolCount: connectedIpPoolCount,
+          );
+          if (!needsUpgrade) {
+            disconnectLabel = switchLabel;
+          }
+        }
       } else {
-        final selectedParent = locationsStore.findParent(selectedLocation!);
-        final selectedCountry =
-            selectedParent?.getName(context) ?? selectedLocation.getName(context);
-        final selectedCity = selectedParent != null ? selectedLocation.getName(context) : '';
+        final country = parentLocation?.getName(context) ?? displayLocation?.getName(context) ?? '';
+        final city = parentLocation != null ? displayLocation?.getName(context) ?? '' : '';
+        final ipType = displayLocation?.ipType;
+        final ipPoolCount =
+            (isConnected ? connectedLocation?.nodeCount : displayLocation?.nodeCount) ?? 0;
+        final countryIcon = displayLocation != null
+            ? CircleFlag(displayLocation.countryCode, size: 32)
+            : const SizedBox(width: 32, height: 32);
+        final serviceQuality = ipType == IPType.residential
+            ? LocaleKeys.residential.tr()
+            : LocaleKeys.highSpeed.tr();
 
-        status = MainIpCardConnected(
-          country: selectedCountry,
-          countryIcon: CircleFlag(selectedLocation.countryCode, size: 32),
-          city: selectedCity,
-          ipAddress: ipInfo ?? '',
-          serviceQuality: connectedServiceQuality,
-          ipPoolCount: connectedIpPoolCount,
-        );
-        if (!needsUpgrade) {
-          disconnectLabel = switchLabel;
+        if (displayLocation == null) {
+          status = const MainIpCardNotConnected();
+        } else if (!isLocationAvailable) {
+          status = const MainIpCardNotConnected();
+        } else if (isConnected) {
+          status = MainIpCardConnected(
+            country: country,
+            countryIcon: countryIcon,
+            city: city,
+            ipAddress: ipInfo ?? '',
+            serviceQuality: serviceQuality,
+            ipPoolCount: ipPoolCount,
+          );
+        } else if (isLoading) {
+          status = MainIpCardConnecting(
+            country: country,
+            countryIcon: countryIcon,
+            serviceQuality: serviceQuality,
+          );
+        } else {
+          status = MainIpCardLocationSelected(
+            country: country,
+            countryIcon: countryIcon,
+            serviceQuality: serviceQuality,
+          );
+        }
+
+        if (displayLocation != null && !isLocationAvailable) {
+          noConnectionTitle = LocaleKeys.locationUnavailableTitle.tr(
+            args: [displayLocation.getName(context)],
+          );
+          noConnectionDescription = LocaleKeys.locationUnavailableSubtitle.tr();
+          connectLabel = LocaleKeys.locationUnavailableAction.tr();
         }
       }
-    } else {
-      final country = parentLocation?.getName(context) ?? displayLocation?.getName(context) ?? '';
-      final city = parentLocation != null ? displayLocation?.getName(context) ?? '' : '';
-      final ipType = displayLocation?.ipType;
-      final ipPoolCount =
-          (isConnected ? connectedLocation?.nodeCount : displayLocation?.nodeCount) ?? 0;
-      final countryIcon = displayLocation != null
-          ? CircleFlag(displayLocation.countryCode, size: 32)
-          : const SizedBox(width: 32, height: 32);
-      final serviceQuality = ipType == IPType.residential
-          ? LocaleKeys.residential.tr()
-          : LocaleKeys.highSpeed.tr();
 
-      if (displayLocation == null) {
-        status = const MainIpCardNotConnected();
-      } else if (!isLocationAvailable) {
-        status = const MainIpCardNotConnected();
-      } else if (isConnected) {
-        status = MainIpCardConnected(
-          country: country,
-          countryIcon: countryIcon,
-          city: city,
-          ipAddress: ipInfo ?? '',
-          serviceQuality: serviceQuality,
-          ipPoolCount: ipPoolCount,
-        );
-      } else if (isLoading) {
-        status = MainIpCardConnecting(
-          country: country,
-          countryIcon: countryIcon,
-          serviceQuality: serviceQuality,
-        );
-      } else {
-        status = MainIpCardLocationSelected(
-          country: country,
-          countryIcon: countryIcon,
-          serviceQuality: serviceQuality,
-        );
+      final onToggle = isLoading
+          ? null
+          : needsUpgrade
+          ? () => _handleUpgradePlan(context)
+          : () => _handleToggleConnection(context, location: targetLocation, intent: intent);
+
+      Future<void> onRefreshIP() async {
+        analyticsStore.logRefreshIP(connectionDisplayStore.connectionIP);
+        selectedLocationStore.value = null;
+        await vpnStore.manageConnection(refreshIP: true);
       }
 
-      if (displayLocation != null && !isLocationAvailable) {
-        noConnectionTitle = LocaleKeys.locationUnavailableTitle.tr(
-          args: [displayLocation.getName(context)],
-        );
-        noConnectionDescription = LocaleKeys.locationUnavailableSubtitle.tr();
-        connectLabel = LocaleKeys.locationUnavailableAction.tr();
+      void onDismissPreview() {
+        selectedLocationStore.value = null;
       }
-    }
 
-    final onToggle = isLoading
-        ? null
-        : needsUpgrade
-        ? () => _handleUpgradePlan(context)
-        : () => _handleToggleConnection(context, location: targetLocation, intent: intent);
+      void onThumbsUp() {
+        showRateConnectionDialog(context, RateConnectionRequestModeEnum.like);
+      }
 
-    Future<void> onRefreshIP() async {
-      analyticsStore.logRefreshIP(connectionDisplayStore.connectionIP);
-      selectedLocationStore.value = null;
-      await vpnStore.manageConnection(refreshIP: true);
-    }
+      void onThumbsDown() {
+        showRateConnectionDialog(context, RateConnectionRequestModeEnum.dislike);
+      }
 
-    void onDismissPreview() {
-      selectedLocationStore.value = null;
-    }
+      final connectionRating = switch (connectionRated) {
+        RateConnectionRequestModeEnum.like => ConnectionRating.thumbsUp,
+        RateConnectionRequestModeEnum.dislike => ConnectionRating.thumbsDown,
+        _ => ConnectionRating.none,
+      };
 
-    void onThumbsUp() {
-      showRateConnectionDialog(context, RateConnectionRequestModeEnum.like);
-    }
-
-    void onThumbsDown() {
-      showRateConnectionDialog(context, RateConnectionRequestModeEnum.dislike);
-    }
-
-    final connectionRating = switch (connectionRated) {
-      RateConnectionRequestModeEnum.like => ConnectionRating.thumbsUp,
-      RateConnectionRequestModeEnum.dislike => ConnectionRating.thumbsDown,
-      _ => ConnectionRating.none,
-    };
-
-    return MainIpCard(
-      status: status,
-      connectLabel: connectLabel,
-      disconnectLabel: disconnectLabel,
-      connectingLabel: vpnStatus == VpnConnectionStatus.disconnecting
-          ? LocaleKeys.disconnecting.tr()
-          : LocaleKeys.connecting.tr(),
-      noConnectionTitle: noConnectionTitle,
-      noConnectionDescription: noConnectionDescription,
-      connectionRatingLabel: LocaleKeys.rateConnection.tr(),
-      onConnect: onToggle,
-      onDisconnect: onToggle,
-      onRefreshIp: onRefreshIP,
-      onThumbsUp: onThumbsUp,
-      onThumbsDown: onThumbsDown,
-      onDismissPreview: onDismissPreview,
-      onSwitchCountry: onToggle,
-      refreshIpTooltip: LocaleKeys.refreshIP.tr(),
-      connectionRating: connectionRating,
-    );
-  }
+      return MainIpCard(
+        status: status,
+        connectLabel: connectLabel,
+        disconnectLabel: disconnectLabel,
+        connectingLabel: vpnStatus == VpnConnectionStatus.disconnecting
+            ? LocaleKeys.disconnecting.tr()
+            : LocaleKeys.connecting.tr(),
+        noConnectionTitle: noConnectionTitle,
+        noConnectionDescription: noConnectionDescription,
+        connectionRatingLabel: LocaleKeys.rateConnection.tr(),
+        onConnect: onToggle,
+        onDisconnect: onToggle,
+        onRefreshIp: onRefreshIP,
+        onThumbsUp: onThumbsUp,
+        onThumbsDown: onThumbsDown,
+        onDismissPreview: onDismissPreview,
+        onSwitchCountry: onToggle,
+        refreshIpTooltip: LocaleKeys.refreshIP.tr(),
+        connectionRating: connectionRating,
+      );
+    },
+  );
 }
 
 Future<void> _handleToggleConnection(
