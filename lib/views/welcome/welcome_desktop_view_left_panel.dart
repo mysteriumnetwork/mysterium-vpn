55203426da05c4922f8dcd9cf983b0f0124cc134
Kristijan Mitrikjeski <kmitrikjeski@gmail.com>
2026-04-16 18:19:40 +0200

    Move agreements and remove legacy layouts
    Refactor UI components: moved agreements.dart from views/consent to lib/components and updated imports. Removed several legacy layout/header/sheet files (base_layout, desktop_page_header, page_header, sheet_scaffold) and view wrappers (consent_view, informational_view) as part of layout simplification. Simplified request_tunnel_permissions_dialog to always use the bottom-sheet variant and adjusted its import path. Also removed AppVersion usage/import from the welcome desktop left panel.

--- lib/views/welcome/welcome_desktop_view_left_panel.dart
+++ lib/views/welcome/welcome_desktop_view_left_panel.dart
@@ -2,7 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:mysterium_vpn/common/styles/style.dart';
 import 'package:mysterium_vpn/common/utils/utils.dart';
-import 'package:mysterium_vpn/components/app_version.dart';
 import 'package:mysterium_vpn/components/easy_button.dart';
 import 'package:mysterium_vpn/components/login_headlines.dart';
 import 'package:mysterium_vpn/components/unauthenticated_header.dart';
@@ -28,7 +27,6 @@ class WelcomeDesktopViewLeftPanel extends StatelessWidget {
             text: LocaleKeys.signIn.tr(),
             onPressed: onSignInPressed,
           ),
-          const Align(alignment: Alignment.centerLeft, child: AppVersion()),
         ],
       ).paddingDirectional(
         horizontal: getMediaWidth(context) * 0.02,
